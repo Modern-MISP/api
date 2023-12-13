31ae9a124d7a3e9bbbb6e3d35a1cb2227338ea47
Timo <unjky@student.kit.edu>
2023-12-13 16:19:48 +0000

    feat: added return type annotations and updated settings for auto-formatting

--- app/routers/AuthKey.py
+++ app/routers/AuthKey.py
@@ -1,40 +1,39 @@
-from pydantic import BaseModel
-from typing import List, Annotated
-from fastapi import FastAPI, HTTPException
+from typing import List, Union
+
 import User
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 class AuthKey(BaseModel):
-    id: str
-    uuid: str
-    authkey_start: str
-    authkey_end: str
-    created: str
-    expiration: str
-    read_only: bool
-    user_id: str
-    comment: str
-    allowed_ips: str  # Stringified JSON Array of IP addresses
-    last_used: str
-    unique_ips: List[str]
-    authkey_raw: str
+    id: str = ""
+    uuid: str = ""
+    authkey_start: str = ""
+    authkey_end: str = ""
+    created: str = ""
+    expiration: str = ""
+    read_only: bool = False
+    user_id: str = ""
+    comment: str = ""
+    allowed_ips: str = ""  # Stringified JSON Array of IP addresses
+    last_used: str = ""
+    unique_ips: List[str] = [""]
+    authkey_raw: str = ""
 
 
 app = FastAPI()
 
 
 @app.get("/auth_keys/")
-async def auth_keys_get():  # Request Body in die Klammern
+async def auth_keys_get() -> Union[AuthKey, User.User]:  # Request Body in die Klammern
     return {AuthKey(), User.User()}  # return body in nach return
 
 
 @app.post("/auth_keys/")
-async def auth_keys_post(
-    authkey: AuthKey,
-):
+async def auth_keys_post(authkey: AuthKey) -> Union[AuthKey, User.User]:
     return {{AuthKey(), User.User()}}
 
 
 @app.post("/auth_keys/add/{UserId")
-async def auth_keys_add_User(UserId: str, authKey: AuthKey):
+async def auth_keys_add_User(UserId: str, authKey: AuthKey) -> AuthKey:
     return AuthKey()
