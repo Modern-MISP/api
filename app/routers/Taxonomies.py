6dffb8d423c60e85d8b909a8938b44e85f0f0e51
unjky <unjky@student.kit.edu>
2024-01-06 19:28:57 +0100

    fix: formatter performed

--- app/routers/Taxonomies.py
+++ app/routers/Taxonomies.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
-from typing import List, Annotated
+
 from fastapi import APIRouter
+
 
 class Taxonomies(BaseModel):
     id: str = ""
@@ -12,20 +13,17 @@ class Taxonomies(BaseModel):
     required: bool
     highlighted: bool
 
+
 router = APIRouter(prefix="/taxonomies")
 
-#Returns all taxonomies
+
+# Returns all taxonomies
 @router.get("/")
 async def get_taxonomies():
-    return {
-        Taxonomies(),
-        {
-            "total_count": int,
-            "current_count": int
-        }
-    }
+    return {Taxonomies(), {"total_count": int, "current_count": int}}
 
-#Get Taxonomy by ID (deprecated)
+
+# Get Taxonomy by ID (deprecated)
 @router.get("/view/{taxonomy_id_parameter}")
 async def get_taxonomy_by_id_depr():
     return {
@@ -35,25 +33,27 @@ async def get_taxonomy_by_id_depr():
             "expanded": str,
             "description": str,
             "exclusive_predicate": bool,
-            "existing_tag": bool
-        }
+            "existing_tag": bool,
+        },
     }
 
-#Get Taxonomy by ID (new)
+
+# Get Taxonomy by ID (new)
 @router.get("/{taxonomy_id_parameter}")
 async def get_taxonomy_by_id():
-    return{
+    return {
         Taxonomies(),
         {
             "tag": str,
             "expanded": str,
             "description": str,
             "exclusive_predicate": bool,
-            "existing_tag": bool
-        }
+            "existing_tag": bool,
+        },
     }
 
-#Enable Taxonomy
+
+# Enable Taxonomy
 @router.post("/enable/{taxonomiesId}")
 async def enable_taxonomies():
     return {
@@ -62,10 +62,11 @@ async def enable_taxonomies():
         "success": bool,
         "name": str,
         "message": str,
-        "url": str
+        "url": str,
     }
 
-#Disable Taxonomies
+
+# Disable Taxonomies
 @router.post("/disable/{taxonomiesId}")
 async def disable_taxonomies():
     return {
@@ -74,45 +75,40 @@ async def disable_taxonomies():
         "success": bool,
         "name": str,
         "message": str,
-        "url": str
+        "url": str,
     }
 
-#Update Taxonomies. Deprecated
+
+# Update Taxonomies. Deprecated
 @router.post("/taxonomies/update")
 async def update_taxonomies_depr():
-    return {
-        "saved": bool,
-        "success": bool,
-        "name": str,
-        "message": str,
-        "url": str
-    }
+    return {"saved": bool, "success": bool, "name": str, "message": str, "url": str}
 
-#Update Taxonomies. New
+
+# Update Taxonomies. New
 @router.put("/taxonomies")
 async def update_taxonomies():
-    return {
-        "saved": bool,
-        "success": bool,
-        "name": str,
-        "message": str,
-        "url": str
-    }
+    return {"saved": bool, "success": bool, "name": str, "message": str, "url": str}
 
-#Get a Taxonomy extended with Tags used in events and attributes
+
+# Get a Taxonomy extended with Tags used in events and attributes
 @router.get("/taxonomy_tags/{taxonomyId}")
 async def get_taxonomy_extanded():
-    return {Taxonomies(),
-        "entries"[{
-            "tag": str,
-            "expanded": str,
-            "exclusive_predicate": bool,
-            "description": str,
-            "existing_tag": str,
-            "events": int,
-            "attributes": int
-            }]
-        }
+    return {
+        Taxonomies(),
+        "entries"[
+            {
+                "tag": str,
+                "expanded": str,
+                "exclusive_predicate": bool,
+                "description": str,
+                "existing_tag": str,
+                "events": int,
+                "attributes": int,
+            }
+        ],
+    }
+
 
 @router.get("export/{taxonomyId}")
 async def export_taxonomy():
@@ -121,17 +117,11 @@ async def export_taxonomy():
         "description": str,
         "version": int,
         "exclusive": bool,
-        "predicates": [{
-            "description": str,
-            "value": str,
-            "expanded": str
-        }],
-        "values":[{
-            "predicate": str,
-            "entry":[{
-                "value": str,
-                "expanded": str,
-                "description": str
-            }]
-        }]
+        "predicates": [{"description": str, "value": str, "expanded": str}],
+        "values": [
+            {
+                "predicate": str,
+                "entry": [{"value": str, "expanded": str, "description": str}],
+            }
+        ],
     }
