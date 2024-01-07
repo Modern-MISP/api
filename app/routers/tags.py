02e12ff418281192fbf8b8c91b5656a24eee07ca
unjky <unjky@student.kit.edu>
2024-01-07 22:04:38 +0100

    feat: added 'tag' endpoints logic

---
+++ app/routers/tags.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from fastapi import APIRouter, Depends  # , HTTPException
+from sqlalchemy.orm import Session
+
+from ..database import get_db
+
+# from ..models.tag import Tag
+from ..schemas.tag_schema import TagSchema
+
+router = APIRouter(prefix="/tags")
+
+
+@router.get("/", response_model=List[TagSchema])
+async def get_tags(db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.get("/view/{tagId}", response_model=List[TagSchema])
+@router.get("/{tagId}", response_model=List[TagSchema])
+async def view_tag(tag_id: str, db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.post("/add", response_model=List[TagSchema])
+@router.post("/", response_model=List[TagSchema])
+async def add_tag(db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.delete("/delete/{tagId}", response_model=List[TagSchema])
+@router.delete("/{tagId}", response_model=List[TagSchema])
+async def delete_tag(tag_id: str, db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.post("/edit/{tagId}", response_model=List[TagSchema])
+@router.put("/{tagId}", response_model=List[TagSchema])
+async def edit_tag(tag_id: str, db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.get("search/{tagSearchTerm}", response_model=List[TagSchema])
+async def search_tags(tagSearchTerm: str, db: Session = Depends(get_db)) -> None:
+    pass
