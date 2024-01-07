a5076e570f2376bb5b3954df5ff2f99937706412
unjky <unjky@student.kit.edu>
2024-01-07 21:13:44 +0100

    feat: added 'object' endpoints

---
+++ app/routers/Objects.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from fastapi import APIRouter, Depends  # , HTTPException
+from sqlalchemy.orm import Session
+
+from ..database import get_db
+
+# from ..models.object import Object
+from ..schemas.object_schema import ObjectSchema
+
+router = APIRouter(prefix="/objects")
+
+
+@router.post("/restsearch", response_model=List[ObjectSchema])
+async def restsearch(db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.post("/add/{eventId}/{objectTemplateId}", response_model=List[ObjectSchema])
+async def add_object(
+    event_id: str, object_template_id: str, db: Session = Depends(get_db)
+) -> None:
+    pass
+
+
+@router.get("/view/{objectId}", response_model=List[ObjectSchema])
+@router.get("/{objectId}")
+async def get_feed_details(object_id: str, db: Session = Depends(get_db)) -> None:
+    pass
+
+
+@router.delete("/delete/{objectId}/{hardDelete}", response_model=List[ObjectSchema])
+@router.delete("/{objectId}/{hardDelete}")
+async def delete_object(
+    objectId: str, hardDelete: bool, db: Session = Depends(get_db)
+) -> None:
+    pass
