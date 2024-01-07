7e3a05055dc037efb05f9c3eaf659e8ce9d3b487
Maximilian Supp <uqney@student.kit.edu>
2024-01-07 17:59:25 +0100

    feat: added functions to events.py and galaxies.py

--- app/routers/events.py
+++ app/routers/events.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List, Union
+from fastapi import APIRouter
+from . import attributes, objects, tags
 
 
 class Events(BaseModel):
     id: str = ""
-    org_id: str = ""
+    org_id: str = ""  # owner org
     distribution: str = ""
-    orgc_id: str = ""
+    orgc_id: str = ""  # creator org
     uuid: str = ""
     date: str = ""
     published: bool = False
@@ -22,6 +24,96 @@ class Events(BaseModel):
     disable_correlation: bool = False
     extends_uuid: str = ""
     event_creator_email: str = ""
+    chryprographicKey: List[str] = [""]
 
 
-app = FastAPI()
+class EventReport(BaseModel):
+    id: str = ""
+    uuid: str = ""
+    event_id: str = ""
+    name: str = ""
+    content: str = ""
+    distribution: str = ""
+    sharing_group_id: str = ""
+    timestamp: str = ""
+    deleted: bool
+
+
+router = APIRouter(prefix="/events")
+
+
+@router.post("/restSearch")
+async def events_restSearch() -> (
+    Union[
+        Events,
+        attributes.Attributes,
+        EventReport.EventReport,
+        objects.Objects,
+        tags.Tags,
+    ]
+):
+    return {
+        Events(),
+        attributes.Attributes(),
+        EventReport.EventReport(),
+        objects.Objects(),
+        tags.Tags(),
+    } - {Events.sighting_timestamp, tags.org_id, tags.inherited}
+
+
+@router.post("/add")
+@router.post("")
+async def events_post():
+    return
+
+
+@router.put("/{eventId}")
+async def events_put():
+    return
+
+
+@router.delete("/delete/{eventId}")
+@router.delete("/{eventId}")
+async def events_delete():
+    return
+
+
+@router.get("")
+async def events_get():
+    return
+
+
+@router.post("/index")
+async def events_index():
+    return
+
+
+@router.get("/view/{eventId}")
+@router.get("/{eventId}")
+async def events_getById():
+    return
+
+
+@router.post("/publish/{eventId}")
+async def events_publish():
+    return
+
+
+@router.post("/unpublish/{eventId}")
+async def events_unpublish():
+    return
+
+
+@router.post("/addTag/{eventId}/{tagId}/local:{local}")
+async def events_addTag():
+    return
+
+
+@router.post("/removeTag/{eventId}/{tagId}")
+async def events_removeTag():
+    return
+
+
+@router.post("/freeTextImport/{eventId}")
+async def events_freeTextImport():
+    return
