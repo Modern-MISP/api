49f22b572f901e3778719a847320ba081a521388
Maximilian Supp <uqney@student.kit.edu>
2024-01-07 17:59:25 +0100

    feat: added functions to events.py and galaxies.py

--- app/routers/Galaxies.py
+++ app/routers/Galaxies.py
@@ -1,7 +1,6 @@
-from typing import List
-
-from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
+from fastapi import APIRouter
 
 
 class Galaxies(BaseModel):
@@ -16,4 +15,48 @@ class Galaxies(BaseModel):
     kill_chain_order: List[str] = [""]
 
 
-app = FastAPI()
+router = APIRouter(prefix="/galaxies")
+
+
+@router.get("/galaxies")
+async def galaxies_get():
+    return
+
+
+@router.post("/galaxies")
+async def galaxies_post():
+    return
+
+
+@router.get("/galaxies/view/{galaxyId}")
+@router.get("/galaxies/{galaxyId}")
+async def galaxies_getById():
+    return
+
+
+@router.post("/galaxies/update")
+async def galaxies_update():
+    return
+
+
+@router.delete("/galaxies/delete/{galaxyId}")
+@router.delete("/galaxies/{galaxyId}")
+async def galaxies_delete():
+    return
+
+
+@router.post("/galaxies/import")
+async def galaxies_import():
+    return
+
+
+@router.post("/galaxies/export/{galaxyId}")
+async def galaxies_export():
+    return
+
+
+@router.post(
+    "/galaxies/attachCluster/{attachTargetId}/{attachTargetType}/local:{local}"
+)
+async def galaxies_attachCluster():
+    return
