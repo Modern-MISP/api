cb40ff8e16cfc9f21e619f7edf2a21633d7fb275
AextraGit <wunderleaaron@gmail.com>
2024-01-09 14:14:50 +0100

    fix: Added classes taxonomy_schema, user_setting_schema; filled them with content and contributed to better readability through separation

--- app/routers/auth_key.py
+++ app/routers/auth_key.py
@@ -1,34 +1,24 @@
 from typing import List
 
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
 from . import user
-
-
-class AuthKey(BaseModel):
-    id: str = ""
-    uuid: str = ""
-    authkey_start: str = ""
-    authkey_end: str = ""
-    created: str = ""
-    expiration: str = ""
-    read_only: bool = False
-    user_id: str = ""
-    comment: str = ""
-    allowed_ips: str = ""  # Stringified JSON Array of IP addresses
-    last_used: str = ""
-    unique_ips: List[str] = [""]
-    authkey_raw: str = ""
+from ..schemas.auth_key_schema import (AuthKey)
+from ..database import get_db
 
 
 router = APIRouter(prefix="/auth_keys")
 
 
 # query and return a list of existing authkey objects and associated users
-@router.get("/")
-async def auth_keys_get():  # Request Body into brackets
-    return {AuthKey(), user.User()}  # return body in after return
+@router.get("/", response_model=List[AuthKey])
+async def auth_keys_get(db: Session = Depends(get_db)) -> List[AuthKey]:  # Request Body into brackets
+    try:
+        auth_keys = db.query(AuthKey).all()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) #Return of User still missing. Will be implemented later.
+    return {auth_keys}  # return body in after return
 
 
 # search auth keys
--- app/routers/user_settings.py
+++ app/routers/user_settings.py
@@ -1,52 +1,41 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException
 
-
-class UserSettings(BaseModel):
-    id: str = ""
-    setting: str = ""
-    value: dict = {
-        "widget": str,
-        "position": {"x": int, "y": int, "width": int, "height": int},
-    }
-    user_id: str = ""
-    timestamp: str = ""
-
+from ..schemas.user_setting_schema import UserSettings
 
 router = APIRouter(prefix="/user_settings")
 
 
 # Returns all User Settings
 @router.get("/")
-async def get_user_settings():  # Request Body into brackets
+async def get_user_settings() -> UserSettings():  # Request Body into brackets
     return {UserSettings()}  # return body in after return
 
 
 # Search all User Settings
 @router.post("/")
-async def search_user_settings():
+async def search_user_settings() -> UserSettings():
     return {UserSettings()}
 
 
 # Get User Setting by Id
 @router.get("/{UserSettingsId}")
-async def view_user_settings():
+async def view_user_settings() -> UserSettings():
     return UserSettings()
 
 
 # Sets User Setting
 @router.post("/setSetting/{userId/{userSettingName}")
-async def set_user_settings():
+async def set_user_settings() -> UserSettings():
     return UserSettings()
 
 
 # Get User Setting by ID
 @router.get("/getSetting/{userId}/{userSettingName}")
-async def get_auth_key_by_id():
+async def get_auth_key_by_id() -> UserSettings():
     return UserSettings
 
 
 # Delete AuthKey by ID
 @router.delete("/{userSettingsId")
-async def delete_user_settings():
+async def delete_user_settings() -> UserSettings():
     return {"saved": bool, "success": bool, "name": str, "message": str, "url": str}
