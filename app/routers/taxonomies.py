06234259233583f9b000117f51643b3f558f6e17
AextraGit <wunderleaaron@gmail.com>
2024-01-09 23:11:31 +0100

    fix: Had an issue with app/routers/taxonomies.py. Deleted file and restoring it with this commit

---
+++ app/routers/taxonomies.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Header
+
+from app.schemas.taxonomies.enable_disable_taxonomy_out import TaxonomyAbleSchema
+from app.schemas.taxonomies.export_taxonomies_out import TaxonomyExportSchema
+from app.schemas.taxonomies.get_taxonomy_by_id_out import TaxonomyEntrySchema
+from app.schemas.taxonomies.get_taxonomy_out import TaxonomyViewSchema
+from app.schemas.taxonomies.update_taxonomy_out import TaxonomyUpdateSchema
+
+router = APIRouter(prefix="/taxonomies")
+
+
+# Returns all taxonomies
+@router.get("/")
+async def get_taxonomies() -> list[TaxonomyViewSchema]:
+    return TaxonomyViewSchema()
+
+
+# Get Taxonomy by ID
+@router.get("/view/{taxonomy_id_parameter}", deprecated=True)  # deprecated
+@router.get("/{taxonomy_id_parameter}")
+async def get_taxonomy_by_id_depr() -> TaxonomyEntrySchema:
+    return TaxonomyEntrySchema()
+
+
+# Enable Taxonomy
+@router.post("/enable/{taxonomiesId}")
+async def enable_taxonomies() -> TaxonomyAbleSchema:
+    return TaxonomyAbleSchema()
+
+
+# Disable Taxonomies
+@router.post("/disable/{taxonomiesId}")
+async def disable_taxonomies() -> TaxonomyAbleSchema:
+    return TaxonomyAbleSchema()
+
+
+# Update Taxonomies.
+@router.post("/taxonomies/update", deprecated=True)  # Deprecated
+@router.put("/taxonomies")
+async def update_taxonomies_depr() -> TaxonomyUpdateSchema:
+    return TaxonomyUpdateSchema()
+
+
+# Get a Taxonomy extended with Tags used in events and attributes
+@router.get("/taxonomy_tags/{taxonomyId}")
+async def get_taxonomy_extended() -> TaxonomyEntrySchema:
+    return TaxonomyEntrySchema()
+
+
+@router.get("export/{taxonomyId}")
+async def export_taxonomy():
+    return TaxonomyExportSchema()
