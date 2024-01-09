6ac5fe553da4cb1d90e8ee9cfe639c8517b9aa07
AextraGit <wunderleaaron@gmail.com>
2024-01-09 16:20:31 +0100

    fix: added auth_key_schema class

--- app/routers/auth_key.py
+++ app/routers/auth_key.py
@@ -4,22 +4,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from . import user
-
-
-class AuthKey(BaseModel):
-    id: str = ""
-    uuid: str = ""
-    authkey_start: str = ""
-    authkey_end: str = ""
-    created: str = ""
-    expiration: str = ""
-    read_only: bool = False
-    user_id: str = ""
-    comment: str = ""
-    allowed_ips: str = ""  # Stringified JSON Array of IP addresses
-    last_used: str = ""
-    unique_ips: List[str] = [""]
-    authkey_raw: str = ""
+from ..schemas.auth_key_schema import AuthKey
 
 
 router = APIRouter(prefix="/auth_keys")
