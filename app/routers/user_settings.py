39be16ecb48f5f3f108065576ffd77e6e3e12199
AextraGit <wunderleaaron@gmail.com>
2024-01-10 18:21:36 +0100

    feat: added user_setting and taxonomies schemas

--- app/routers/user_settings.py
+++ app/routers/user_settings.py
@@ -1,42 +1,50 @@
 from fastapi import APIRouter
 
-from app.schemas.user_setting.user_setting_schema import UserSettings
+from ..schemas.user_setting.get_get_id_user_setting_out import UserSettingGet
+from ..schemas.user_setting.search_user_setting_out import UserSettingSearch
+from ..schemas.user_setting.search_user_setting_in import UserSettingSearchIn
+from ..schemas.user_setting.view_user_setting_out import UserSettingView
+from ..schemas.user_setting.set_user_setting_in import UserSettingSetIn
+from ..schemas.user_setting.set_user_setting_out import UserSettingSet
+from ..schemas.user_setting.delete_user_setting_out import UserSettingDelete
 
 router = APIRouter(prefix="/user_settings", tags=["user_settings"])
 
 
 # Returns all User Settings
 @router.get("/")
-async def get_user_settings() -> UserSettings():  # Request Body into brackets
-    return {UserSettings()}  # return body in after return
+async def get_user_settings() -> list[UserSettingGet]:  # Request Body into brackets
+    return UserSettingGet()  # return body in after return
 
 
 # Search all User Settings
 @router.post("/")
-async def search_user_settings() -> UserSettings():
-    return {UserSettings()}
+async def search_user_settings(user_setting_in: UserSettingSearchIn) -> list[UserSettingGet]:
+    return UserSettingSearch()
 
 
 # Get User Setting by Id
+@router.get("/view/{UserSettingsId", deprecated=True)  # Deprecated
 @router.get("/{UserSettingsId}")
-async def view_user_settings() -> UserSettings():
-    return UserSettings()
+async def view_user_settings() -> UserSettingView:
+    return UserSettingView()
 
 
 # Sets User Setting
-@router.post("/setSetting/{userId/{userSettingName}")
-async def set_user_settings() -> UserSettings():
-    return UserSettings()
+@router.post("/setSetting/{userId}/{userSettingName}")
+async def set_user_settings(user_setting_in: UserSettingSetIn) -> UserSettingSet:
+    return UserSettingSet()
 
 
 # Get User Setting by ID
-@router.get("/getSetting/{userId}/{userSettingName}")
-async def get_auth_key_by_id() -> UserSettings():
-    return UserSettings
+@router.get("/getSetting/{userId}/{userSettingName}")  # should be without getSetting
+@router.get("/{userid}/{userSettingName}")
+async def get_auth_key_by_id() -> UserSettingGet:
+    return UserSettingGet()
 
 
 # Delete AuthKey by ID
 @router.delete("/{userSettingsId")
 @router.delete("/delete/{userSettingsId", deprecated=True)  # Deprecated
-async def delete_user_settings() -> UserSettings():
-    return {"saved": bool, "success": bool, "name": str, "message": str, "url": str}
+async def delete_user_settings() -> UserSettingDelete:
+    return UserSettingDelete()
