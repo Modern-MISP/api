e02ec40c552bb4bf318d04d9b5218f9f680945f0
Maximilian Supp <uqney@student.kit.edu>
2024-01-10 19:53:07 +0100

    fix: adjust structure of endpoints for better readability

--- app/routers/galaxies.py
+++ app/routers/galaxies.py
@@ -5,52 +5,58 @@ from sqlalchemy.orm import Session
 
 from ..database import get_db
 from ..models.galaxy import Galaxy
-from ..schemas.galaxy_schema import GalaxySchema
+# from ..schemas.galaxy_schema import GalaxySchema
 
-router = APIRouter(prefix="/galaxies")
-
-
-@router.get("", response_model=List[GalaxySchema])
-async def galaxies_get(db: Session = Depends(get_db)) -> List[Galaxy]:
-    pass
+router = APIRouter(prefix="/galaxies", tags=["galaxies"])
 
 
-@router.post("", response_model=List[GalaxySchema])
-async def galaxies_post(db: Session = Depends(get_db)) -> List[Galaxy]:
-    pass
+# -- Delete
 
 
-@router.get("/view/{galaxyId}", response_model=GalaxySchema)
-@router.get("/{galaxyId}", response_model=GalaxySchema)
-async def galaxies_getById(db: Session = Depends(get_db)) -> Galaxy:
-    pass
-
-
-@router.post("/update", response_model=str)
-async def galaxies_update(db: Session = Depends(get_db)) -> str:
-    pass
-
-
-@router.delete("/delete/{galaxyId}", response_model=str)
+@router.delete("/delete/{galaxyId}")
 @router.delete("/{galaxyId}")
 async def galaxies_delete(db: Session = Depends(get_db)) -> str:
-    pass
+    return ""
+
+
+# -- Get
+
+
+@router.get("/")
+async def galaxies_get(db: Session = Depends(get_db)) -> List[Galaxy]:
+    return []
+
+
+@router.get("/view/{galaxyId}")
+@router.get("/{galaxyId}")
+async def galaxies_getById(db: Session = Depends(get_db)) -> Galaxy:
+    return Galaxy
+
+
+# -- Post
+
+
+@router.post("")
+async def galaxies_post(db: Session = Depends(get_db)) -> List[Galaxy]:
+    return []
+
+
+@router.post("/update")
+async def galaxies_update(db: Session = Depends(get_db)) -> str:
+    return ""
 
 
 # import GalaxyClusterSchema and Galaxy
-@router.post("/import")  # , response_model=List[GalaxyClusterSchema]
+@router.post("/import")
 async def galaxies_import(db: Session = Depends(get_db)):  # -> List[GalaxyCluster]
-    pass
+    return ""
 
 
-@router.post("/export/{galaxyId}")  # , response_model=GalaxyClusterSchema
+@router.post("/export/{galaxyId}")
 async def galaxies_export(db: Session = Depends(get_db)):  # -> GalaxyCluster:
-    pass
+    return ""
 
 
-@router.post(
-    "/attachCluster/{attachTargetId}/{attachTargetType}/local:{local}",
-    response_model=str,
-)
+@router.post("/attachCluster/{attachTargetId}/{attachTargetType}/local:{local}")
 async def galaxies_attachCluster(db: Session = Depends(get_db)) -> str:
-    pass
+    return ""
