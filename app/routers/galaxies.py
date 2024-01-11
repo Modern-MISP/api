49fc9f677767180df38498a83b0de9784db51fe3
Maximilian Supp <uqney@student.kit.edu>
2024-01-11 18:13:44 +0100

    feat: added schemas for galaxies and updated endpoints

--- app/routers/galaxies.py
+++ app/routers/galaxies.py
@@ -1,11 +1,21 @@
-from typing import List
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..database import get_db
-from ..models.galaxy import Galaxy
-# from ..schemas.galaxy_schema import GalaxySchema
+from ..schemas.galaxies.delete_galaxy_response import GalaxyDeleteResponse
+from ..schemas.galaxies.get_all_galaxies import GalaxiesGetResponse
+from ..schemas.galaxies.get_galaxy_response import GalaxyGetResponse
+from ..schemas.galaxies.search_galaxies_body import GalaxySearchBody
+from ..schemas.galaxies.search_galaxies_response import GalaxySearchResponse
+from ..schemas.galaxies.forece_update_galaxies_response import GalaxyUpdateResponse
+from ..schemas.galaxies.import_galaxies_body import GalaxyImportBody
+from ..schemas.galaxies.import_galaxies_response import GalaxyImportResponse
+from ..schemas.galaxies.export_galaxies_body import GalaxyExportBody
+from ..schemas.galaxies.export_galaxies_response import GalaxyExportResponse
+from ..schemas.galaxies.attach_galaxy_cluster_body import GalaxyAttachClusterBody
+from ..schemas.galaxies.attach_galaxy_cluster_response import (
+    GalaxyAttachClusterResponse,
+)
 
 router = APIRouter(prefix="/galaxies", tags=["galaxies"])
 
@@ -13,50 +23,63 @@ router = APIRouter(prefix="/galaxies", tags=["galaxies"])
 # -- Delete
 
 
-@router.delete("/delete/{galaxyId}")
-@router.delete("/{galaxyId}")
-async def galaxies_delete(db: Session = Depends(get_db)) -> str:
-    return ""
+@router.delete("/delete/{galaxy_id}", deprecated=True)  # deprecated
+@router.delete("/{galaxy_id}")  # new
+async def galaxies_delete(
+    galagxy_id: str, db: Session = Depends(get_db)
+) -> GalaxyDeleteResponse:
+    return GalaxyDeleteResponse
 
 
 # -- Get
 
 
 @router.get("/")
-async def galaxies_get(db: Session = Depends(get_db)) -> List[Galaxy]:
-    return []
+async def galaxies_get(db: Session = Depends(get_db)) -> GalaxiesGetResponse:
+    return GalaxiesGetResponse
 
 
-@router.get("/view/{galaxyId}")
-@router.get("/{galaxyId}")
-async def galaxies_getById(db: Session = Depends(get_db)) -> Galaxy:
-    return Galaxy
+@router.get("/view/{galaxy_id}", deprecated=True)  # deprecated
+@router.get("/{galaxy_id}")  # new
+async def galaxies_getById(db: Session = Depends(get_db)) -> GalaxyGetResponse:
+    return GalaxyGetResponse
 
 
 # -- Post
 
 
 @router.post("")
-async def galaxies_post(db: Session = Depends(get_db)) -> List[Galaxy]:
-    return []
+async def galaxies_post(
+    body: GalaxySearchBody, db: Session = Depends(get_db)
+) -> GalaxySearchResponse:
+    return GalaxySearchResponse
 
 
 @router.post("/update")
-async def galaxies_update(db: Session = Depends(get_db)) -> str:
-    return ""
+async def galaxies_update(db: Session = Depends(get_db)) -> GalaxyUpdateResponse:
+    return GalaxyUpdateResponse
 
 
-# import GalaxyClusterSchema and Galaxy
 @router.post("/import")
-async def galaxies_import(db: Session = Depends(get_db)):  # -> List[GalaxyCluster]
-    return ""
+async def galaxies_import(
+    body: GalaxyImportBody, db: Session = Depends(get_db)
+) -> GalaxyImportResponse:
+    return GalaxyImportResponse
 
 
-@router.post("/export/{galaxyId}")
-async def galaxies_export(db: Session = Depends(get_db)):  # -> GalaxyCluster:
-    return ""
+@router.post("/export/{galaxy_id}")
+async def galaxies_export(
+    galagxy_id: str, body: GalaxyExportBody, db: Session = Depends(get_db)
+) -> GalaxyExportResponse:
+    return GalaxyExportResponse
 
 
-@router.post("/attachCluster/{attachTargetId}/{attachTargetType}/local:{local}")
-async def galaxies_attachCluster(db: Session = Depends(get_db)) -> str:
-    return ""
+@router.post("/attachCluster/{attachTarget_id}/{attachTargetType}/local:{local}")
+async def galaxies_attachCluster(
+    attachTarget_id: str,
+    attachTargetType: str,
+    local: int,
+    body: GalaxyAttachClusterBody,
+    db: Session = Depends(get_db),
+) -> GalaxyAttachClusterResponse:
+    return GalaxyAttachClusterResponse
