1e0a9fb1194a115daa2d303cb962ce3e0b47f833
Maximilian Supp <uqney@student.kit.edu>
2024-01-13 23:10:14 +0100

    feat: updated galaxy schemas

--- app/routers/galaxies.py
+++ app/routers/galaxies.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..database import get_db
-from ..schemas.galaxies.delete_galaxy_response import DeleteGalaxyResponse
-from ..schemas.galaxies.get_all_galaxies import GetAllGalaxiesResponse
+from ..schemas.galaxies.delete_force_update_import_galaxy_response import (
+    DeleteForceUpdateImportGalaxyResponse,
+)
+from ..schemas.galaxies.get_all_search_galaxies_response import (
+    GetAllSearchGalaxiesResponse,
+)
 from ..schemas.galaxies.get_galaxy_response import GetGalaxyResponse
 from ..schemas.galaxies.search_galaxies_body import SearchGalaxiesBody
-from ..schemas.galaxies.search_galaxies_response import SearchGalaxiesResponse
-from ..schemas.galaxies.force_update_galaxies_response import ForceUpdateGalaxyResponse
 from ..schemas.galaxies.import_galaxies_body import ImportGalaxyBody
-from ..schemas.galaxies.import_galaxies_response import ImportGalaxyResponse
 from ..schemas.galaxies.export_galaxies_body import ExportGalaxyBody
 from ..schemas.galaxies.export_galaxies_response import ExportGalaxyResponse
 from ..schemas.galaxies.attach_galaxy_cluster_body import AttachClusterGalaxyBody
@@ -27,16 +28,18 @@ router = APIRouter(prefix="/galaxies", tags=["galaxies"])
 @router.delete("/{galaxy_id}")  # new
 async def galaxies_delete(
     galagxy_id: str, db: Session = Depends(get_db)
-) -> DeleteGalaxyResponse:
-    return DeleteGalaxyResponse()
+) -> DeleteForceUpdateImportGalaxyResponse:
+    return DeleteForceUpdateImportGalaxyResponse()
 
 
 # -- Get
 
 
 @router.get("/")
-async def galaxies_get(db: Session = Depends(get_db)) -> GetAllGalaxiesResponse:
-    return GetAllGalaxiesResponse()
+async def galaxies_get(
+    db: Session = Depends(get_db),
+) -> list[GetAllSearchGalaxiesResponse]:
+    return list[GetAllSearchGalaxiesResponse()]
 
 
 @router.get("/view/{galaxy_id}", deprecated=True)  # deprecated
@@ -51,20 +54,22 @@ async def galaxies_getById(db: Session = Depends(get_db)) -> GetGalaxyResponse:
 @router.post("/")
 async def galaxies_post(
     body: SearchGalaxiesBody, db: Session = Depends(get_db)
-) -> SearchGalaxiesResponse:
-    return SearchGalaxiesResponse()
+) -> GetAllSearchGalaxiesResponse:
+    return GetAllSearchGalaxiesResponse()
 
 
 @router.post("/update")
-async def galaxies_update(db: Session = Depends(get_db)) -> ForceUpdateGalaxyResponse:
-    return ForceUpdateGalaxyResponse
+async def galaxies_update(
+    db: Session = Depends(get_db),
+) -> DeleteForceUpdateImportGalaxyResponse:
+    return DeleteForceUpdateImportGalaxyResponse
 
 
 @router.post("/import")
 async def galaxies_import(
     body: ImportGalaxyBody, db: Session = Depends(get_db)
-) -> ImportGalaxyResponse:
-    return ImportGalaxyResponse()
+) -> DeleteForceUpdateImportGalaxyResponse:
+    return DeleteForceUpdateImportGalaxyResponse()
 
 
 @router.post("/export/{galaxy_id}")
