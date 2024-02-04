972b27567bfbc9f54354cdfacee4106b96df77e3
Maximilian <uqney@student.kit.edu>
2024-02-04 15:21:46 +0100

    feat: add tests for attributes

--- tests/api/routers/test_attributes.py
+++ tests/api/routers/test_attributes.py
@@ -4,14 +4,18 @@ import pytest
 
 from ...environment import client, environment
 from ...generators.attribute_generator import (
-    generate_existing_attribute_id,
-    generate_invalid_attribute_id,
+    generate_existing_id,
     generate_invalid_field_add_attribute_data,
+    generate_invalid_id,
     generate_invalid_required_field_add_attribute_data,
+    generate_invalid_search_attributes_data,
     generate_missing_required_field_add_attribute_data,
-    generate_non_existing_attribute_id,
+    generate_missing_required_field_search_attribute_data,
+    generate_non_existing_id,
     generate_valid_add_attribute_data,
+    generate_valid_delete_selected_attributes_data,
     generate_valid_edit_attribute_data,
+    generate_valid_search_attributes_data,
 )
 
 # --- Test cases ---
@@ -22,7 +26,7 @@ from ...generators.attribute_generator import (
 
 @pytest.fixture(scope="module")
 def add_attribute_valid_data() -> Dict[str, Any]:
-    return generate_valid_add_attribute_data.dict()
+    return generate_valid_add_attribute_data().dict()
 
 
 def test_add_attribute_valid_data(add_attribute_valid_data: Dict[str, Any]) -> None:
@@ -31,17 +35,21 @@ def test_add_attribute_valid_data(add_attribute_valid_data: Dict[str, Any]) -> N
     assert response.status_code == 200
     response_json = response.json()
     for key, value in add_attribute_valid_data.items():
-        response_json[key] == value
+        assert response_json[key] == value
 
 
-@pytest.mark.parametrize(
-    "add_attribute_invalid_data",
-    [
-        generate_invalid_field_add_attribute_data.dict(),
-        generate_invalid_required_field_add_attribute_data.dict(),
-        generate_missing_required_field_add_attribute_data.dict(),
+@pytest.fixture(
+    params=[
+        generate_invalid_field_add_attribute_data().dict(),
+        generate_invalid_required_field_add_attribute_data().dict(),
+        generate_missing_required_field_add_attribute_data().dict(),
     ],
+    scope="module",
 )
+def add_attribute_invalid_data(request: Any) -> Dict[str, Any]:
+    return request.param
+
+
 def test_add_attribute_missing_required_field(add_attribute_invalid_data: Dict[str, Any]) -> None:
     headers = {"authorization": environment.site_admin_user_token}
     response = client.post("/attributes/", json=add_attribute_invalid_data, headers=headers)
@@ -64,28 +72,50 @@ def test_add_attribute_authorization(add_attribute_valid_data: Dict[str, Any]) -
 
 
 @pytest.fixture(scope="module")
-def existing_attribute_id() -> str:
-    return generate_existing_attribute_id()
+def existing_id() -> str:
+    return generate_existing_id()
 
 
-def test_get_existing_attribute(existing_attribute_id: str) -> None:
-    response = client.get("/attributes/" + existing_attribute_id)
+def test_get_existing_attribute(existing_id: str) -> None:
+    response = client.get("/attributes/" + existing_id)
     assert response.status_code == 200
-    assert response.json()["id"] == existing_attribute_id
+    response_json = response.json()
+    assert response_json[0] == "Attribute"
+    attribute = response_json[0]
+    assert attribute["id"] == existing_id
+    for field in attribute:
+        assert "id" in field
+        assert "event_id" in field
+        assert "object_id" in field
+        assert "object_relation" in field
+        assert "category" in field
+        assert "type" in field
+        assert "value" in field
+        assert "to_ids" in field
+        assert "uuid" in field
+        assert "timestamp" in field
+        assert "distribution" in field
+        assert "sharing_group_id" in field
+        assert "comment" in field
+        assert "deleted" in field
+        assert "disable_correlation" in field
+        assert "first_seen" in field
+        assert "last_seen" in field
+        assert "Tag" in field
 
 
-@pytest.mark.parametrize(
-    "invalid_or_non_existing_attribute_ids",
-    [generate_non_existing_attribute_id, generate_invalid_attribute_id],
-    scope="module",
-)
-def test_get_invalid_or_non_existing_attribute(invalid_or_non_existing_attribute_ids: str) -> None:
-    response = client.get("/attributes/" + invalid_or_non_existing_attribute_ids)
+@pytest.fixture(params=[generate_non_existing_id, generate_invalid_id], scope="module")
+def invalid_or_non_existing_ids(request: Any) -> str:
+    request.param
+
+
+def test_get_invalid_or_non_existing_attribute(invalid_or_non_existing_ids: str) -> None:
+    response = client.get("/attributes/" + invalid_or_non_existing_ids)
     assert response.status_code == 404
 
 
-def test_get_attribute_response_format(existing_attribute_id: str) -> None:
-    response = client.get("/attributes/" + existing_attribute_id)
+def test_get_attribute_response_format(existing_id: str) -> None:
+    response = client.get("/attributes/" + existing_id)
     assert response.headers["Content-Type"] == "application/json"
 
 
@@ -97,47 +127,187 @@ def edit_attribute_valid_data() -> Dict[str, Any]:
     return generate_valid_edit_attribute_data().dict()
 
 
-def test_edit_existing_attribute(existing_attribute_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
+def test_edit_existing_attribute(existing_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
     headers = {"authorization": environment.site_admin_user_token}
-    response = client.put("/attributes/" + existing_attribute_id, json=edit_attribute_valid_data, headers=headers)
+    response = client.put("/attributes/" + existing_id, json=edit_attribute_valid_data, headers=headers)
     assert response.status_code == 200
+    response_json = response.json()
+    assert response_json[0] == "Attribute"
+    attribute = response_json["Attribute"][0]
+    assert attribute["id"] == existing_id
+    for key, value in edit_attribute_valid_data.items():
+        assert attribute[key] == value
 
 
 def test_edit_non_existing_attribute(
-    invalid_or_non_existing_attribute_ids: str, edit_attribute_valid_data: Dict[str, Any]
+    invalid_or_non_existing_ids: str, edit_attribute_valid_data: Dict[str, Any]
 ) -> None:
     headers = {"authorization": environment.site_admin_user_token}
-    response = client.put(
-        "/attributes/" + invalid_or_non_existing_attribute_ids, json=edit_attribute_valid_data, headers=headers
-    )
+    response = client.put("/attributes/" + invalid_or_non_existing_ids, json=edit_attribute_valid_data, headers=headers)
     assert response.status_code == 404
 
 
-def test_edit_attribute_response_format(existing_attribute_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
+def test_edit_attribute_response_format(existing_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
     headers = {"authorization": environment.site_admin_user_token}
-    response = client.put("/attributes/" + existing_attribute_id, json=edit_attribute_valid_data, headers=headers)
+    response = client.put("/attributes/" + existing_id, json=edit_attribute_valid_data, headers=headers)
     assert response.headers["Content-Type"] == "application/json"
 
 
-def test_edit_attribute_authorization(existing_attribute_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
+def test_edit_attribute_authorization(existing_id: str, edit_attribute_valid_data: Dict[str, Any]) -> None:
     headers = {"authorization": ""}
-    response = client.put("/attributes/" + existing_attribute_id, json=edit_attribute_valid_data, headers=headers)
+    response = client.put("/attributes/" + existing_id, json=edit_attribute_valid_data, headers=headers)
     assert response.status_code == 401
 
 
 # --- Test delete attribute by id
 
 
-def test_delete_existing_attribute(existing_attribute_id: str) -> None:
-    response = client.delete("/attributes/" + existing_attribute_id)
+def test_delete_existing_attribute(existing_id: str) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.delete("/attributes/" + existing_id, headers=headers)
     assert response.status_code == 200
 
 
-def test_delete_invalid_or_non_existing_attribute(invalid_or_non_existing_attribute_ids: str) -> None:
-    response = client.delete("/attributes/" + invalid_or_non_existing_attribute_ids)
+def test_delete_invalid_or_non_existing_attribute(invalid_or_non_existing_ids: str) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.delete("/attributes/" + invalid_or_non_existing_ids, headers=headers)
     assert response.status_code == 404
 
 
-def test_delete_attribute_response_format(existing_attribute_id: str) -> None:
-    response = client.delete("/attributes/" + existing_attribute_id)
+def test_delete_attribute_response_format(existing_id: str) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.delete("/attributes/" + existing_id, headers=headers)
     assert response.headers["Content-Type"] == "application/json"
+
+
+def test_delete_attribute_authorization(existing_id: str) -> None:
+    headers = {"authorization": ""}
+    response = client.delete("/attributes/" + existing_id, headers=headers)
+    assert response.status_code == 401
+
+
+# --- Test get all attributes
+
+
+def test_get_all_attributes() -> None:
+    response = client.get("/attributes/")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert isinstance(response_json, list)
+    for attribute in response_json:
+        assert "id" in attribute
+        assert "event_id" in attribute
+        assert "object_id" in attribute
+        assert "object_relation" in attribute
+        assert "category" in attribute
+        assert "type" in attribute
+        assert "value" in attribute
+        assert "value1" in attribute
+        assert "value2" in attribute
+        assert "to_ids" in attribute
+        assert "uuid" in attribute
+        assert "timestamp" in attribute
+        assert "distribution" in attribute
+        assert "sharing_group_id" in attribute
+        assert "comment" in attribute
+        assert "deleted" in attribute
+        assert "disable_correlation" in attribute
+        assert "first_seen" in attribute
+        assert "last_seen" in attribute
+
+
+def test_get_all_attributes_response_format() -> None:
+    response = client.get("/attributes/")
+    assert response.headers["Content-Type"] == "application/json"
+
+
+# --- Test delete selected attribute(s)
+
+
+@pytest.fixture()
+def delete_selected_attributes_data() -> Dict[str, Any]:
+    return generate_valid_delete_selected_attributes_data().dict()
+
+
+def test_delete_selected_attributes_from_existing_event(
+    existing_id: str, delete_selected_existing_attributes_data: Dict[str, Any]
+) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.post(
+        "/attributes/deleteSelected/" + existing_id, json=delete_selected_existing_attributes_data, headers=headers
+    )
+    assert response.status_code == 200
+    response_json = response.json()
+    counter_of_selected_attributes = delete_selected_attributes_data["id"].count(" ") + 1
+    if counter_of_selected_attributes == 1:
+        assert response_json["message"] == str(counter_of_selected_attributes) + "attribute deleted"
+    else:
+        assert response_json["message"] == str(counter_of_selected_attributes) + "attributes deleted"
+
+
+def test_delete_selected_attributes_response_format(
+    existing_id: str, delete_selected_existing_attributes_data: Dict[str, Any]
+) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.post(
+        "/attributes/deleteSelected/" + existing_id, json=delete_selected_existing_attributes_data, headers=headers
+    )
+    assert response.headers["Content-Type"] == "application/json"
+
+
+def test_delete_selected_attributes_authorization(
+    existing_id: str, delete_selected_existing_attributes_data: Dict[str, Any]
+) -> None:
+    headers = {"authorization": ""}
+    response = client.post(
+        "/attributes/deleteSelected/" + existing_id, json=delete_selected_existing_attributes_data, headers=headers
+    )
+    assert response.status_code == 401
+
+
+@pytest.fixture(scope="module")
+def search_attribute_valid_data() -> Dict[str, Any]:
+    return generate_valid_search_attributes_data().dict()
+
+
+def test_valid_search_attribute_data(search_attribute_valid_data: Dict[str, Any]) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.post("/attributes/restSearch", json=search_attribute_valid_data, headers=headers)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json[0] == "response"
+    response_json_attribute = response_json["response"][0]
+    assert response_json_attribute == "Attribute"
+    response_json_details = response_json_attribute["Attribute"][0]
+    for key, value in search_attribute_valid_data:
+        assert response_json_details[key] == value
+
+
+@pytest.fixture(
+    params=[
+        generate_invalid_search_attributes_data().dict(),
+        generate_missing_required_field_search_attribute_data().dict(),
+    ]
+)
+def search_attributes_invalid_data(request: Any) -> Dict[str, Any]:
+    return request.param
+
+
+def test_invalid_or_missing_required_field_search_attribute_data(
+    search_attributes_invalid_data: Dict[str, Any],
+) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.post("/attributes/restSearch", json=search_attributes_invalid_data, headers=headers)
+    assert response.status_code == 404
+
+
+def test_search_attributes_response_format(search_attribute_valid_data: Dict[str, Any]) -> None:
+    headers = {"authorization": environment.site_admin_user_token}
+    response = client.post("/attributes/restSearch", json=search_attribute_valid_data, headers=headers)
+    assert response.headers["Content-Type"] == "application/json"
+
+
+def test_search_attributes_authorization(search_attribute_valid_data: Dict[str, Any]) -> None:
+    headers = {"authorization": ""}
+    response = client.post("/attributes/restSearch", json=search_attribute_valid_data, headers=headers)
+    assert response.status_code == 401
--- tests/generators/attribute_generator.py
+++ tests/generators/attribute_generator.py
@@ -1,5 +1,7 @@
 from mmisp.api_schemas.attributes.add_attribute_body import AddAttributeBody
+from mmisp.api_schemas.attributes.delete_selected_attribute_body import DeleteSelectedAttributeBody
 from mmisp.api_schemas.attributes.edit_attribute_body import EditAttributeBody
+from mmisp.api_schemas.attributes.search_attributes_body import SearchAttributesBody
 
 
 def generate_valid_add_attribute_data() -> AddAttributeBody:
@@ -26,15 +28,15 @@ def generate_invalid_field_add_attribute_data() -> AddAttributeBody:
     return AddAttributeBody(value="1.2.3.4", type="ip-src", category=1)
 
 
-def generate_existing_attribute_id() -> str:
+def generate_existing_id() -> str:
     return "1"
 
 
-def generate_non_existing_attribute_id() -> str:
+def generate_non_existing_id() -> str:
     return "0"
 
 
-def generate_invalid_attribute_id() -> str:
+def generate_invalid_id() -> str:
     return "invalid id"
 
 
@@ -47,3 +49,19 @@ def generate_valid_edit_attribute_data() -> EditAttributeBody:
         comment="new_comment",
         disable_correlation=False,
     )
+
+
+def generate_valid_delete_selected_attributes_data() -> DeleteSelectedAttributeBody:
+    return DeleteSelectedAttributeBody(id="1 2 3", allow_hard_delete=False)
+
+
+def generate_valid_search_attributes_data() -> SearchAttributesBody:
+    return SearchAttributesBody(returnFormat="json")
+
+
+def generate_invalid_search_attributes_data() -> SearchAttributesBody:
+    return SearchAttributesBody(returnFormat="invalid format")
+
+
+def generate_missing_required_field_search_attribute_data() -> SearchAttributesBody:
+    return SearchAttributesBody(limit=5)
