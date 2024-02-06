583f0ec1303e2b77af1b5434996d6d222bd40952
Timo <unjky@student.kit.edu>
2024-02-06 15:46:06 +0100

    refactor: added status codes and logging

--- src/mmisp/api/routers/objects.py
+++ src/mmisp/api/routers/objects.py
@@ -30,8 +30,7 @@ router = APIRouter(tags=["objects"])
 
 logging.basicConfig(
     level=logging.INFO  # todo: remove comment
-)  # 'logger.error' for errors on the part of the user, 'logger.exception' for errors on the part of the server
-logger = logging.getLogger(__name__)
+)  # 'logger.error' for errors on part of the user, 'logger.exception' for errors on part of the server
 
 info_format = "%(asctime)s - %(message)s"
 error_format = "%(asctime)s - %(filename)s:%(lineno)d - %(message)s"
