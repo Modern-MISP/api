ab71a91f2739061d154db9e947d3be33a1904653
Maximilian <uqney@student.kit.edu>
2024-02-13 11:35:39 +0100

    feat: added route for adding an event

--- tests/api/routers/test_events.py
+++ tests/api/routers/test_events.py
@@ -3,10 +3,8 @@ from typing import Any, Dict
 import pytest
 from sqlalchemy.orm import Session
 
-from mmisp.api_schemas.events.add_edit_get_event_response import AddEditGetEventDetails
-
 from ...environment import client, environment, get_db
-from ...generators.event_generator import generate_valid_add_event_body
+from ...generators.event_generator import generate_invalid_add_event_body, generate_valid_add_event_body
 
 
 @pytest.fixture(scope="function")
@@ -23,6 +21,11 @@ def add_event_valid_data() -> Dict[str, Any]:
     return generate_valid_add_event_body().dict()
 
 
+@pytest.fixture(scope="module")
+def add_event_invalid_data() -> Dict[str, Any]:
+    return generate_invalid_add_event_body().dict()
+
+
 class TestAddEvent:
     def test_add_event_valid_data(self: "TestAddEvent", add_event_valid_data: Dict[str, Any]) -> None:
         headers = {"authorization": environment.site_admin_user_token}
@@ -30,16 +33,16 @@ class TestAddEvent:
 
         assert response.status_code == 200
         response_json = response.json()
-        for attribute in AddEditGetEventDetails.__dict__.keys():
-            assert attribute in response_json["Event"]
+        assert response_json["Event"]["info"] == add_event_valid_data["info"]
+        assert response_json["Event"]["date"] == "2024-02-13"
 
     def test_add_event_invalid_data(self: "TestAddEvent", add_event_invalid_data: Dict[str, Any]) -> None:
         headers = {"authorization": environment.site_admin_user_token}
         response = client.post("/events", json=add_event_invalid_data, headers=headers)
 
-        if not add_event_invalid_data["id"]:
+        if not add_event_invalid_data["info"]:
             assert response.status_code == 400
-        elif not isinstance(add_event_invalid_data["id"], str):
+        elif not isinstance(add_event_invalid_data["info"], str):
             assert response.status_code == 403
 
     def test_add_event_response_format(self: "TestAddEvent", add_event_valid_data: Dict[str, Any]) -> None:
