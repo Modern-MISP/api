10069327ec561a819e2bcb28e6d20fdbf340f809
Timo <unjky@student.kit.edu>
2024-02-16 14:25:01 +0100

    feat: implement 'sightings' (MA-65) (wip)

--- src/mmisp/api/routers/sightings.py
+++ src/mmisp/api/routers/sightings.py
@@ -1,41 +1,27 @@
-import logging
+import time
 from typing import Annotated, Any
 
-# from fastapi import APIRouter, Depends, HTTPException, Path, status
 from fastapi import APIRouter, Depends, Path, status
-
-# from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from mmisp.api.auth import Auth, AuthStrategy, Permission, authorize
 from mmisp.api_schemas.sightings.create_sighting_body import SightingCreateBody
 from mmisp.api_schemas.sightings.delete_sighting_response import SightingDeleteResponse
-from mmisp.api_schemas.sightings.get_sighting_response import SightingGetResponse
+from mmisp.api_schemas.sightings.get_sighting_response import (
+    SightingAttributesResponse,
+    SightingGetResponse,
+    SightingOrganisationResponse,
+)
 from mmisp.db.database import get_db
-
-# from mmisp.db.models.sighting import Sighting
+from mmisp.db.models.attribute import Attribute
+from mmisp.db.models.organisation import Organisation
+from mmisp.db.models.sighting import Sighting
 from mmisp.util.partial import partial
 
 # from mmisp.util.request_validations import check_existence_and_raise
 
 router = APIRouter(tags=["sightings"])
-logging.basicConfig(level=logging.INFO)
-
-info_format = "%(asctime)s - %(message)s"
-error_format = "%(asctime)s - %(filename)s:%(lineno)d - %(message)s"
-
-info_handler = logging.StreamHandler()
-info_handler.setLevel(logging.INFO)
-info_handler.setFormatter(logging.Formatter(info_format))
-
-error_handler = logging.StreamHandler()
-error_handler.setLevel(logging.ERROR)
-error_handler.setFormatter(logging.Formatter(error_format))
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(info_handler)
-logger.addHandler(error_handler)
 
 
 # Sorted according to CRUD
@@ -49,7 +35,7 @@ logger.addHandler(error_handler)
     description="Add a new sighting.",
 )
 async def add_sighting(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.SIGHTING]))],
+    # auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.SIGHTING]))],  #!
     db: Annotated[Session, Depends(get_db)],
     body: SightingCreateBody,
 ) -> list[dict[str, Any]]:
@@ -182,34 +168,69 @@ async def get_sightings_at_index_depr(
 # --- endpoint logic ---
 
 
-async def _add_sighting(
-    db: Session,
-    body: SightingCreateBody,
-) -> list[dict[str, Any]]:
-    return [{"key": "hold"}]
+async def _add_sighting(db: Session, body: SightingCreateBody) -> list[dict[str, Any]]:
+    # response: SightingGetResponse = list[SightingAttributesResponse]
+    responses = []
+
+    for value in body.values:
+        attributes = db.execute(select(Attribute).where(Attribute.value == value)).scalars().all()
+
+        for attribute in attributes:
+            sighting = Sighting(
+                # attribute_id=int(attribute.id),
+                attribute_id=1,
+                # event_id=int(attribute.event_id),
+                event_id=1,
+                # org_id=int(db.get(Event, attribute.event_id).org_id),
+                org_id=1,
+                date_sighting=_create_timestamp(),
+                # source=body.source if body.source else None,
+                # type=int(body.filters.type) if str(body.filters.type) else None,
+            )
+            db.add(sighting)
+            db.commit()
+            db.refresh(sighting)
+
+            organisation = db.get(Organisation, sighting.org_id)
+
+            organisation_response = SightingOrganisationResponse(
+                id=organisation.id if organisation else None,
+                uuid=organisation.uuid if organisation else None,
+                name=organisation.name if organisation else None,
+            )
+
+            responses.append(
+                SightingAttributesResponse(
+                    id=str(sighting.id),
+                    uuid=sighting.uuid,
+                    attribute_id=str(sighting.attribute_id),
+                    event_id=str(sighting.event_id),
+                    org_id=str(sighting.org_id),
+                    date_sighting=str(sighting.date_sighting),
+                    source=sighting.source,
+                    type=str(sighting.type),
+                    organisation=organisation_response,
+                )
+            )
+
+    return SightingGetResponse(root=[response.__dict__ for response in responses])
 
 
-async def _add_sightings_at_index(
-    db: Session,
-    attribute_id: str,
-    body: SightingCreateBody,
-) -> dict[str, Any]:
+async def _add_sightings_at_index(db: Session, attribute_id: str, body: SightingCreateBody) -> dict[str, Any]:
     return {"key": "hold"}
 
 
-async def _get_sightings_at_index(
-    db: Session,
-    event_id: str,
-) -> dict[str, Any]:
+async def _get_sightings_at_index(db: Session, event_id: str) -> dict[str, Any]:
     return {"key": "hold"}
 
 
-async def _delete_sighting(
-    db: Session,
-    sighting_id: str,
-) -> dict[str, Any]:
+async def _delete_sighting(db: Session, sighting_id: str) -> dict[str, Any]:
     return {"key": "hold"}
 
 
 async def _get_sightings(db: Session) -> list[dict[str, Any]]:
     return [{"key": "hold"}]
+
+
+def _create_timestamp() -> int:
+    return int(time.time())
