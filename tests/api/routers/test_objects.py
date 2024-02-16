17af81d5125d2e0cc22d426366118d65c1a143b9
Timo <unjky@student.kit.edu>
2024-02-16 10:33:50 +0100

    feat: mock additional database fields for ForeignKey-relationships (MA-62)

--- tests/api/routers/test_objects.py
+++ tests/api/routers/test_objects.py
@@ -1,13 +1,14 @@
-from typing import Any, Dict
+import time
+from typing import Any
 
 import pytest
 
 from mmisp.db.models.event import Event
 from mmisp.db.models.object import ObjectTemplate
+from mmisp.db.models.organisation import Organisation
 from mmisp.db.models.sharing_group import SharingGroup
-
-from ...environment import client, environment, get_db
-from ...generators.object_generator import (
+from tests.environment import client, environment, get_db
+from tests.generators.object_generator import (
     generate_random_search_query,
     generate_search_query,
     generate_specific_search_query,
@@ -25,19 +26,19 @@ from ...generators.object_generator import (
         generate_valid_random_object_data().dict(),
     ]
 )
-def object_data(request: Any) -> Dict[str, Any]:
+def object_data(request: Any) -> dict[str, Any]:
     return request.param
 
 
 class TestAddObject:
-    def test_add_object_to_event(self: "TestAddObject", object_data: Dict[str, Any]) -> None:
+    def test_add_object_to_event(self: "TestAddObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -45,13 +46,31 @@ class TestAddObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -65,14 +84,14 @@ class TestAddObject:
         assert int(response_data["object"]["event_id"]) == event_id
         assert int(response_data["object"]["template_id"]) == object_template_id
 
-    def test_add_object_response_format(self: "TestAddObject", object_data: Dict[str, Any]) -> None:
+    def test_add_object_response_format(self: "TestAddObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -80,13 +99,31 @@ class TestAddObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -96,7 +133,7 @@ class TestAddObject:
         assert response.headers["Content-Type"] == "application/json"
         assert "object" in response.json()
 
-    def test_add_object_to_event_authorization(self: "TestAddObject", object_data: Dict[str, Any]) -> None:
+    def test_add_object_to_event_authorization(self: "TestAddObject", object_data: dict[str, Any]) -> None:
         headers = {"authorization": ""}
         response = client.post("/feeds", json=object_data, headers=headers)
         assert response.status_code == 401
@@ -112,12 +149,12 @@ class TestAddObject:
         generate_random_search_query().dict(),
     ]
 )
-def search_data(request: Any) -> Dict[str, Any]:
+def search_data(request: Any) -> dict[str, Any]:
     return request.param
 
 
 class TestSearchObject:
-    def test_search_objects_with_filters(self: "TestSearchObject", search_data: Dict[str, Any]) -> None:
+    def test_search_objects_with_filters(self: "TestSearchObject", search_data: dict[str, Any]) -> None:
         response = client.post("/objects/restsearch", json=search_data)
         assert response.status_code == 200
 
@@ -131,14 +168,14 @@ class TestSearchObject:
             assert "distribution" in obj["object"] != ""
             assert "comment" in obj["object"] != ""
 
-    def test_search_objects_response_format(self: "TestSearchObject", search_data: Dict[str, Any]) -> None:
+    def test_search_objects_response_format(self: "TestSearchObject", search_data: dict[str, Any]) -> None:
         response = client.post("/objects/restsearch", json=search_data)
         assert response.headers["Content-Type"] == "application/json"
         response_data = response.json()
         assert "response" in response_data
         assert isinstance(response_data["response"], list)
 
-    def test_search_objects_data_integrity(self: "TestSearchObject", search_data: Dict[str, Any]) -> None:
+    def test_search_objects_data_integrity(self: "TestSearchObject", search_data: dict[str, Any]) -> None:
         response = client.post("/objects/restsearch", json=search_data)
         response_data = response.json()
         for obj in response_data["response"]:
@@ -149,14 +186,14 @@ class TestSearchObject:
 
 
 class TestGetObjectInfo:
-    def test_get_object_details_valid_id(self: "TestGetObjectInfo", object_data: Dict[str, Any]) -> None:
+    def test_get_object_details_valid_id(self: "TestGetObjectInfo", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -164,13 +201,31 @@ class TestGetObjectInfo:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -194,14 +249,14 @@ class TestGetObjectInfo:
         assert "comment" in object_data
         assert "sharing_group_id" in object_data
 
-    def test_get_object_details_response_format(self: "TestGetObjectInfo", object_data: Dict[str, Any]) -> None:
+    def test_get_object_details_response_format(self: "TestGetObjectInfo", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -209,13 +264,31 @@ class TestGetObjectInfo:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -237,14 +310,14 @@ class TestGetObjectInfo:
         response = client.get(f"/objects/{object_id}", headers=headers)
         assert response.status_code == 404
 
-    def test_get_object_details_data_integrity(self: "TestGetObjectInfo", object_data: Dict[str, Any]) -> None:
+    def test_get_object_details_data_integrity(self: "TestGetObjectInfo", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -252,13 +325,31 @@ class TestGetObjectInfo:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -277,14 +368,14 @@ class TestGetObjectInfo:
 
 
 class TestDeleteObject:
-    def test_delete_object_hard_delete(self: "TestDeleteObject", object_data: Dict[str, Any]) -> None:
+    def test_delete_object_hard_delete(self: "TestDeleteObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -292,13 +383,31 @@ class TestDeleteObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -318,14 +427,14 @@ class TestDeleteObject:
         assert response_data["saved"] is True
         assert response_data["success"] is True
 
-    def test_delete_object_soft_delete(self: "TestDeleteObject", object_data: Dict[str, Any]) -> None:
+    def test_delete_object_soft_delete(self: "TestDeleteObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -333,13 +442,31 @@ class TestDeleteObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -366,14 +493,14 @@ class TestDeleteObject:
         assert response_delete.status_code == 404
         assert "detail" in response_delete.json()
 
-    def test_delete_object_invalid_hard_delete(self: "TestDeleteObject", object_data: Dict[str, Any]) -> None:
+    def test_delete_object_invalid_hard_delete(self: "TestDeleteObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -381,13 +508,31 @@ class TestDeleteObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
@@ -399,17 +544,17 @@ class TestDeleteObject:
         response_data = response.json()
         object_id = response_data["object"]["id"]
         response_delete = client.delete(f"/objects/{object_id}/invalid_value", headers=headers)
-        assert response_delete.status_code == 400
+        assert response_delete.status_code == 422
         assert "detail" in response_delete.json()
 
-    def test_delete_object_no_authorization(self: "TestDeleteObject", object_data: Dict[str, Any]) -> None:
+    def test_delete_object_no_authorization(self: "TestDeleteObject", object_data: dict[str, Any]) -> None:
         db = get_db()
 
         sharing_group = SharingGroup(
             name="test_group", releasability="", organisation_uuid="", org_id=1, sync_user_id=1
         )
         db.add(sharing_group)
-        db.commit()
+        db.flush()
         db.refresh(sharing_group)
         object_data["sharing_group_id"] = sharing_group.id
         for attribute in object_data["attributes"]:
@@ -417,13 +562,31 @@ class TestDeleteObject:
 
         object_template = ObjectTemplate(name="test_template", user_id=1, org_id=1, version=100)
         db.add(object_template)
-        db.commit()
+        db.flush()
         db.refresh(object_template)
 
-        event = Event(info="test")
+        organisation = Organisation(
+            name="test",
+            date_created=str(int(time.time())),
+            date_modified=str(int(time.time())),
+        )
+        db.add(organisation)
+        db.flush()
+        db.refresh(organisation)
+
+        event = Event(
+            org_id=organisation.id,
+            orgc_id=organisation.id,
+            info="test",
+            date=str(int(time.time())),
+            analysis="test",
+            event_creator_email="XXXXXXXXXXXXX",
+        )
         db.add(event)
-        db.commit()
+        db.flush()
         db.refresh(event)
+
+        db.commit()
 
         object_template_id = object_template.id
         event_id = event.id
