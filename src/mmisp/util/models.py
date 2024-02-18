f8455be51495f58fc5d47bc04d6b741ab665b59e
Jakob Christoph Vogler <jakob@jcvogler.com>
2024-02-18 09:23:48 +0100

    fix: consider enums in models util

--- src/mmisp/util/models.py
+++ src/mmisp/util/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import TypeVar
 
 from pydantic import BaseModel
@@ -7,9 +8,7 @@ T = TypeVar("T", bound=BaseModel)
 
 def update_record(record: T, update: dict) -> None:
     for key, value in update.items():
-        new_value = getattr(record, key)
+        if value is None:
+            continue
 
-        if value is not None:
-            new_value = value
-
-        setattr(record, key, new_value)
+        setattr(record, key, value if not isinstance(value, Enum) else value.value)
