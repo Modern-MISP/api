0b56ff22b00dea1f37ad86463fee14badc1073a9
Maximilian <uqney@student.kit.edu>
2024-02-18 20:12:24 +0100

    refactor: updated galaxies tests and routers

---
+++ tests/api/helpers/galaxy_helper.py
@@ -0,0 +1,63 @@
+from mmisp.api_schemas.galaxies.export_galaxies_response import ExportGalaxyGalaxyElement
+from mmisp.api_schemas.galaxies.get_galaxy_response import GetGalaxyClusterResponse
+from mmisp.api_schemas.galaxies.import_galaxies_body import ImportGalaxyBody, ImportGalaxyGalaxy
+
+
+def get_valid_import_galaxy_body(tag_name: str, galaxy_id: int, org_id: int, galaxy_uuid: str) -> list[dict]:
+    body = ImportGalaxyBody(
+        GalaxyCluster=GetGalaxyClusterResponse(
+            collection_uuid="",
+            type="test type",
+            value="test",
+            tag_name=tag_name,
+            description="",
+            galaxy_id=galaxy_id,
+            source="https://github.com/mitre/cti",
+            authors=["Me"],
+            version="1",
+            distribution="1",
+            sharing_group_id="1",
+            org_id=org_id,
+            orgc_id=org_id,
+            default=False,
+            locked=False,
+            extends_uuid="",
+            extends_version="1",
+            published=False,
+            deleted="False",
+            GalaxyElement=[ExportGalaxyGalaxyElement(key="key", value="value")],
+        ),
+        Galaxy=ImportGalaxyGalaxy(uuid=galaxy_uuid),
+    )
+    response_list = [body.dict()]
+    return response_list
+
+
+def get_invalid_import_galaxy_body(tag_name: str, galaxy_id: int, org_id: int) -> list[dict]:
+    body = ImportGalaxyBody(
+        GalaxyCluster=GetGalaxyClusterResponse(
+            collection_uuid="",
+            type="",
+            value="test",
+            tag_name=tag_name,
+            description="",
+            galaxy_id=galaxy_id,
+            source="https://github.com/mitre/cti",
+            authors=["Me"],
+            version="1",
+            distribution="1",
+            sharing_group_id="1",
+            org_id=org_id,
+            orgc_id=org_id,
+            default=False,
+            locked=False,
+            extends_uuid="",
+            extends_version="1",
+            published=False,
+            deleted="False",
+            GalaxyElement=[ExportGalaxyGalaxyElement(key="key", value="value")],
+        ),
+        Galaxy=ImportGalaxyGalaxy(uuid=""),
+    )
+    response_list = [body.dict()]
+    return response_list
