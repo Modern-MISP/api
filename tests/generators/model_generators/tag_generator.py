75fdb8c0dcd86f4292e2c564ad879e9cce430fae
Maximilian <uqney@student.kit.edu>
2024-02-18 20:13:06 +0100

    refactor: updated tag model_generator

--- tests/generators/model_generators/tag_generator.py
+++ tests/generators/model_generators/tag_generator.py
@@ -3,7 +3,7 @@ from time import time
 from mmisp.db.models.tag import Tag
 
 
-def generate_tag_() -> Tag:
+def generate_tag() -> Tag:
     """These fields need to be set manually: org_id, user_id"""
     return Tag(
         name=f"test tag {time()}",
