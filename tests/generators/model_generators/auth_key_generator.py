e980a6822d0026a12cfb7b6ec71d6c983fb920ff
Jakob Christoph Vogler <jakob@jcvogler.com>
2024-03-11 19:22:59 +0100

    chore: update docstring

--- tests/generators/model_generators/auth_key_generator.py
+++ tests/generators/model_generators/auth_key_generator.py
@@ -3,7 +3,7 @@ from mmisp.util.crypto import hash_secret
 
 
 def generate_auth_key() -> AuthKey:
-    """These fields need to be set manually: user_id, [authkey]"""
+    """These fields need to be set manually: user_id, [authkey, authkey_start, authkey_end]"""
     return AuthKey(
         authkey=hash_secret("test"),
         authkey_start="test",
