d9dbf154133909a210e4def0520ced8b01f434af
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-06-14 10:06:30 +0000

    changed order

--- src/mmisp/api/routers/users.py
+++ src/mmisp/api/routers/users.py
@@ -23,6 +23,16 @@ async def add_user(
     TODO
 ) : return await _add_user(db, user)
 
+@router.get("/users/view/me.json", response_model=partial(UsersViewMeResponse))
+@router.get("/users/view/me", response_model=partial(UsersViewMeResponse))
+async def get_logged_in_user_info(
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID))], db: Annotated[Session, Depends(get_db)]
+) -> dict:
+    user = await db.get(User, auth.user_id)
+    organisation = await db.get(Organisation, auth.org_id)
+    role = await db.get(Role, auth.role_id)
+
+    return {"User": user.__dict__, "Organisation": organisation.__dict__, "Role": role.__dict__}
 
 @router.get(
     "/users/view/all",
@@ -72,19 +82,6 @@ async def delete_user_token(
 async def update_user(
     TODO
 ) : return await _update_user(db, userID)
-
-
-
-@router.get("/users/view/me.json", response_model=partial(UsersViewMeResponse))
-@router.get("/users/view/me", response_model=partial(UsersViewMeResponse))
-async def get_logged_in_user_info(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID))], db: Annotated[Session, Depends(get_db)]
-) -> dict:
-    user = await db.get(User, auth.user_id)
-    organisation = await db.get(Organisation, auth.org_id)
-    role = await db.get(Role, auth.role_id)
-
-    return {"User": user.__dict__, "Organisation": organisation.__dict__, "Role": role.__dict__}
 
 # --- endpoint logic ---
 
