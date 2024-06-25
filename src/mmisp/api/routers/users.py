cf7e66601d442ddd4597a2dc259d2b4e7ef13e4c
uhuqu <uhuqu@student.kit.edu>
2024-06-25 17:07:59 +0200

    fix: /users (add new user)

--- src/mmisp/api/routers/users.py
+++ src/mmisp/api/routers/users.py
@@ -1,26 +1,28 @@
+import time
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
-from mmisp.api_schemas.users import UsersViewMeResponse, UserAttributesResponse
+from mmisp.api.auth import Auth, AuthStrategy, Permission, authorize, check_permissions
+from mmisp.api_schemas.users import AddUserBody, AddUserResponse, UsersViewMeResponse
 from mmisp.db.database import Session, get_db
 from mmisp.db.models.organisation import Organisation
 from mmisp.db.models.role import Role
 from mmisp.db.models.user import User
 from mmisp.util.partial import partial
 
-from ..auth import Auth, AuthStrategy, authorize
-
 router = APIRouter(tags=["users"])
+
 
 @router.post(
     "/users",
-    #response_model=UserAttributesResponse,
     summary="Add new user",
 )
 async def add_user(
-    TODO
-) :
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID))],
+    db: Annotated[Session, Depends(get_db)],
+    body: AddUserBody,
+) -> AddUserResponse:
     """
     Adds a new user with the given details.
 
@@ -29,12 +31,13 @@ async def add_user(
     - Data representing the new user to be added
 
     - The current database
-    
+
     Output:
 
     - Data representing the attributes of the new user
     """
-    return await _add_user(db, user)
+    return await _add_user(auth=auth, db=db, body=body)
+
 
 @router.get("/users/view/me.json", response_model=partial(UsersViewMeResponse))
 @router.get("/users/view/me", response_model=partial(UsersViewMeResponse))
@@ -47,7 +50,7 @@ async def get_logged_in_user_info(
     Input:
 
     - Authentication details of the logged in user
-    
+
     Output:
 
     - Information about the logged in user
@@ -58,33 +61,31 @@ async def get_logged_in_user_info(
 
     return {"User": user.__dict__, "Organisation": organisation.__dict__, "Role": role.__dict__}
 
+
 @router.get(
     "/users/view/all",
     summary="Get all users",
 )
-async def get_all_users(
-   TODO
-) :
+async def get_all_users(TODO):
     """
     Retrieves a list of all users.
 
     Input:
 
     - The current database
-    
+
     Output:
 
     - List containing all users
     """
     return await _get_all_users(db)
 
+
 @router.get(
     "/users/view/{userId}",
     summary="Get a user by id",
 )
-async def get_user_by_id(
-   TODO
-) :
+async def get_user_by_id(TODO):
     """
     Retrieves a user specified by id.
 
@@ -93,21 +94,20 @@ async def get_user_by_id(
     - ID of the user to get
 
     - The current database
-    
+
     Output:
 
     - Data representing the attributes of the searched user
     """
     return await _get_user(db, userId)
 
+
 @router.delete(
     "/users/{userId}",
-    #response_model=UserAttributesResponse,
+    # response_model=UserAttributesResponse,
     summary="Delete a user",
 )
-async def delete_user(
-    TODO
-) :
+async def delete_user(TODO):
     """
     Deletes a user by their ID.
 
@@ -116,21 +116,20 @@ async def delete_user(
     - ID of the user to delete
 
     - The current database
-    
+
     Output:
 
     - Response indicating success or failure
     """
     return await _delete_user(db, userID)
 
+
 @router.delete(
     "/users/tokens/{userId}",
-    #response_model=UserAttributesResponse,
+    # response_model=UserAttributesResponse,
     summary="Delete a users login token",
 )
-async def delete_user_token(
-    TODO
-) :
+async def delete_user_token(TODO):
     """
     Deletes a users login token by their ID.
 
@@ -139,22 +138,20 @@ async def delete_user_token(
     - ID of the user with the token to delete
 
     - The current database
-    
+
     Output:
 
     - Response indicating success or failure
     """
     return await _delete_user_token(db, userID)
 
+
 @router.put(
     "/users/{userId}",
-
-    #response_model=UserAttributesResponse,
+    # response_model=UserAttributesResponse,
     summary="Update a user",
 )
-async def update_user(
-    TODO
-) :
+async def update_user(TODO):
     """
     Updates an existing user by their ID.
 
@@ -165,25 +162,79 @@ async def update_user(
     - Updated data for the user
 
     - The current database
-    
+
     Output:
-    
+
     - Data representing the updated attributes of the user
     """
     return await _update_user(db, userID)
 
+
 # --- endpoint logic ---
 
-async def _add_user(db: Session, user: User) : return None
-#Adds a new user and generates a new single-time password
 
-async def _get_all_users(db: Session) : return None
+async def _add_user(auth: Auth, db: Session, body: AddUserBody) -> AddUserResponse:
+    if not (
+        await check_permissions(db, auth, [Permission.SITE_ADMIN])
+        and await check_permissions(db, auth, [Permission.ADMIN])
+    ):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
-async def _get_user(db: Session, userID: str) : return None
+    user = User(
+        org_id=body.org_id,
+        email=body.email,
+        password=body.password,
+        # autoalert: bool,
+        invited_by=auth.user_id,
+        gpgkey=body.gpgkey,
+        # certif_public: str,
+        termsaccepted=body.termsaccepted,
+        role_id=body.role_id,
+        change_pw=True,
+        # contactalert: bool,
+        disabled=body.disabled,
+        # expiration: datetime,
+        # current_login: str,
+        last_login=int(time.time()),
+        force_logout=False,
+        date_created=int(time.time()),
+        date_modified=int(time.time()),
+        external_auth_required=False,
+        # external_auth_key: str,
+        # last_api_access: str,
+        notification_daily=body.notification_daily,
+        notification_weekly=body.notification_weekly,
+        notification_monthly=body.notification_monthly,
+        totp=body.totp,
+        hotp_counter=None,
+        last_pw_change=int(time.time()),
+    )
 
-async def _delete_user(db: Session, userID: str) : return None
+    db.add(user)
 
-async def _delete_user_token(db: Session, userID: str) : return None
+    await db.commit()
+    await db.refresh(user)
+    return AddUserResponse(id=user.id)
 
-async def _update_user(db: Session, userID: str) : return None
 
+# Adds a new user and generates a new single-time password
+
+
+async def _get_all_users(db: Session):
+    return None
+
+
+async def _get_user(db: Session, userID: str):
+    return None
+
+
+async def _delete_user(db: Session, userID: str):
+    return None
+
+
+async def _delete_user_token(db: Session, userID: str):
+    return None
+
+
+async def _update_user(db: Session, userID: str):
+    return None
--- tests/api/routers/test_users.py
+++ tests/api/routers/test_users.py
@@ -1,3 +1,8 @@
+from sqlalchemy.future import select
+
+from mmisp.db.models.user import User
+
+
 def test_users_me(site_admin_user_token, site_admin_user, client) -> None:
     response = client.get("/users/view/me", headers={"authorization": site_admin_user_token})
 
@@ -6,7 +11,38 @@ def test_users_me(site_admin_user_token, site_admin_user, client) -> None:
     assert json.get("User").get("id") == str(site_admin_user.id)
 
 
-def test_users_create(site_admin_user_token, client) -> None:
-    respone = client.post("/users", headers={"authorization": site_admin_user_token})
+def test_users_create(site_admin_user_token, site_admin_user, client, db) -> None:
+    email = "test@automated.com"
+    response = client.post(
+        "/users",
+        headers={"authorization": site_admin_user_token},
+        json={
+            "email": email,
+            "password": "test",
+            "name": "test",
+            "role_id": 0,
+            "org_id": site_admin_user.org_id,
+            "nids_sid": "12345",
+            "disabled": False,
+            "totp": None,
+            "contact_enabled": False,
+            "notification_daily": False,
+            "notification_weekly": True,
+            "notification_monthly": True,
+            "termsaccepted": False,
+            "gpgkey": "Test",
+        },
+    )
 
-    assert respone.status_code == 200
+    json = response.json()
+    assert response.status_code == 200
+
+    query = select(User).where(User.id == json.get("id"))
+    user = db.execute(query).scalars().first()
+    assert user is not None
+    assert user.email == email
+
+    db.delete(user)
+    db.commit()
+    user = db.execute(query).scalars().first()
+    assert db.execute(query).scalars().first() is None
