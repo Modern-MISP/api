24b0ca8cddbb62daad04ba97456d9280ccfb6da2
uhuqu <uhuqu@student.kit.edu>
2024-06-25 11:47:16 +0200

    feat: added test_users_create

--- tests/api/routers/test_users.py
+++ tests/api/routers/test_users.py
@@ -4,3 +4,9 @@ def test_users_me(site_admin_user_token, site_admin_user, client) -> None:
     assert response.status_code == 200
     json = response.json()
     assert json.get("User").get("id") == str(site_admin_user.id)
+
+
+def test_users_create(site_admin_user_token, client) -> None:
+    respone = client.post("/users", headers={"authorization": site_admin_user_token})
+
+    assert respone.status_code == 200
