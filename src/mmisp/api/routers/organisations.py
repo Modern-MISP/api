fe64036eb624d35ad18ae74574e50691001dccac
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-07-07 10:57:27 +0000

    add missing import

--- src/mmisp/api/routers/organisations.py
+++ src/mmisp/api/routers/organisations.py
@@ -5,7 +5,7 @@ from sqlalchemy.future import select
 
 from mmisp.api.auth import Auth, AuthStrategy, Permission, authorize, check_permissions
 from mmisp.api_schemas.standard_status_response import StandardStatusIdentifiedResponse
-from mmisp.api_schemas.organisations import GetOrganisationResponse
+from mmisp.api_schemas.organisations import GetOrganisationResponse, DeleteForceUpdateOrganisationResponse
 from mmisp.db.database import Session, get_db
 from mmisp.db.models.organisation import Organisation
 from mmisp.util.partial import partial
