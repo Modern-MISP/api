a7e90a34458af0e3b0c5b5d931a8bccaca6b4704
Matthias David Ziegler <ulpov@student.kit.edu>
2024-07-15 10:58:21 +0200

    added admin logs index endpoit & tests

--- src/mmisp/api/routers/logs.py
+++ src/mmisp/api/routers/logs.py
@@ -1,25 +1,51 @@
-from typing import Annotated, List
+from typing import Annotated, Any, List
 
 from fastapi import APIRouter, Depends, status
 from sqlalchemy import select
 
 from mmisp.api.auth import Auth, AuthStrategy, authorize
+from mmisp.api_schemas.logs import LogsRequest
 from mmisp.db.database import Session, get_db
 from mmisp.db.models.log import Log
+from mmisp.workflows.fastapi import log_to_json_dict
 
 router = APIRouter(tags=["logs"])
 
 
 @router.post(
-    "/logs/index",
+    "/admin/logs/index",
     status_code=status.HTTP_201_CREATED,
     summary="Add object to event",
     description="Add a new object to a specific event using a template.",
 )
 async def add_object(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, []))], db: Annotated[Session, Depends(get_db)]
-) -> List[dict]:
-    db_result = db.execute(select(Log).order_by(Log.created.desc()).limit(50))
-    logs = db_result.scalars().all()
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, []))],
+    db: Annotated[Session, Depends(get_db)],
+    request: LogsRequest,
+) -> List[Any]:
+    logs = await query_logs(request, db)
 
-    pass
+    result = []
+
+    for log in logs:
+        log_json = log_to_json_dict(log)
+        result.append(log_json)
+
+    return result
+
+
+async def query_logs(request: LogsRequest, db: Session) -> List[Log]:
+    query = select(Log)
+
+    if request.model:
+        query = query.filter(Log.model == request.model)
+    if request.action:
+        query = query.filter(Log.action == request.action)
+    if request.model_id:
+        query = query.filter(Log.model_id == request.model_id)
+
+    query = query.offset((request.page - 1) * request.limit).limit(request.limit)
+    query = query.order_by(Log.created.desc())
+
+    db_result = await db.execute(query)
+    return db_result.scalars().all()
--- tests/api/routers/test_logs.py
+++ tests/api/routers/test_logs.py
@@ -1,5 +1,37 @@
-def test_logs_index(db, site_admin_user_token, client) -> None:
-    headers = {"authorization": site_admin_user_token}
-    response = client.post("/events", headers=headers)
+from mmisp.db.models.log import Log
+from mmisp.api_schemas.logs import LogsRequest
+from mmisp.db.database import Session
+import pytest
 
-    assert 1
+def test_logs_index(db, site_admin_user_token, client, insert_test_logs) -> None:
+    json = LogsRequest(
+        model="Workflow",
+        model_id=12345678
+    ).dict()
+    headers = {"authorization": site_admin_user_token}
+    response = client.post("/admin/logs/index", headers=headers, json=json)
+
+    assert response.status_code = 200
+    assert "12345678" in response.test
+
+@pytest.fixture()
+def insert_test_logs(db: Session):
+    log = Log(
+        model="Workflow",
+        model_id=12345678,
+        action="action",
+        user_id=1,
+        email="admin@admin.test",
+        org="awhlud",
+        change="Blablabla",
+        ip="127.000.000.1"
+    )
+    db.add(log)
+    db.commit()
+    db.refresh(log)
+
+    yield log
+
+    db.delete(log)
+    db.commit()
+    db.refresh(log)
