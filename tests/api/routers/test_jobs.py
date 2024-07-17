5016ecb771ccdd9c6a76749d77ef72f2e6056cfa
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-07-17 16:15:10 +0000

    fix: test get job

--- tests/api/routers/test_jobs.py
+++ tests/api/routers/test_jobs.py
@@ -6,7 +6,7 @@ from mmisp.api.config import config
 
 @respx.mock
 def test_get_job(site_admin_user_token, client) -> None:
-    route = respx.get(f"{config.WORKER_URL}/jobs/1/status").mock(return_value=Response(200, json={}))
+    route = respx.get(f"{config.WORKER_URL}/jobs/1/result").mock(return_value=Response(200, json={}))
 
     response = client.get("/jobs/1", headers={"authorization": site_admin_user_token})
 
