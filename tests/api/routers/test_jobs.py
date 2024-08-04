c78efb3e1c7f0317e0b3b58cfed251d94aeae7c1
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-08-04 10:20:56 +0000

    feat: job tests

--- tests/api/routers/test_jobs.py
+++ tests/api/routers/test_jobs.py
@@ -14,3 +14,43 @@ def test_get_job(site_admin_user_token, client) -> None:
     json = response.json()
     assert json == {}
     assert route.called
+
+
+@respx.mock
+def test_get_job_unfinished(site_admin_user_token, client) -> None:
+    route = respx.get(f"{config.WORKER_URL}/job/1/result").mock(return_value=Response(409, json={}))
+
+    response = client.get("/jobs/1", headers={"authorization": site_admin_user_token})
+
+    assert response.status_code == 409
+    assert route.called
+
+
+@respx.mock
+def test_get_job_no_result(site_admin_user_token, client) -> None:
+    route = respx.get(f"{config.WORKER_URL}/job/1/result").mock(return_value=Response(204, json={}))
+
+    response = client.get("/jobs/1", headers={"authorization": site_admin_user_token})
+
+    assert response.status_code == 204
+    assert route.called
+
+
+@respx.mock
+def test_get_job_no_result(site_admin_user_token, client) -> None:
+    route = respx.get(f"{config.WORKER_URL}/job/1/result").mock(return_value=Response(204, json={}))
+
+    response = client.get("/jobs/1", headers={"authorization": site_admin_user_token})
+
+    assert response.status_code == 204
+    assert route.called
+
+
+@respx.mock
+def test_get_job_unexpected_error(site_admin_user_token, client) -> None:
+    route = respx.get(f"{config.WORKER_URL}/job/1/result").mock(return_value=Response(500, json={}))
+
+    response = client.get("/jobs/1", headers={"authorization": site_admin_user_token})
+
+    assert response.status_code == 500
+    assert route.called
