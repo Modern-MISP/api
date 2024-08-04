d61586c249a05392f38cb78318cb8bd510173b60
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-08-04 09:35:18 +0000

    feat: hopefully the last batch of user tests

--- tests/api/routers/test_users.py
+++ tests/api/routers/test_users.py
@@ -1,12 +1,18 @@
 import json
 from time import time
 
+import pytest
 from icecream import ic
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 
 from mmisp.db.models.user import User
 from mmisp.db.models.user_setting import UserSetting
+from mmisp.api.auth import encode_token
+
+@pytest.fixture
+def read_only_user_token(view_only_user):
+    return encode_token(view_only_user.id)
 
 
 def test_users_me(site_admin_user_token, site_admin_user, client) -> None:
@@ -423,3 +429,68 @@ def test_update_user_attributes(site_admin_user_token, client, db, view_only_use
     for key, value in body.items():
         if key in response_user:
             assert response_user[key] == value
+
+
+def test_users_create_no_permission(read_only_user_token, site_admin_user, site_admin_role, client, db) -> None:
+    email = "test@automated.com" + str(time())
+    name = "test_user" + str(time())
+
+    response = client.post(
+        "/users",
+        headers={"authorization": read_only_user_token},
+        json={
+            "authkey": "test",
+            "email": email,
+            "password": "test",
+            "name": name,
+            "role": site_admin_role.id,
+            "org": site_admin_user.org_id,
+            "nids_sid": "12345",
+            "disabled": False,
+            "contactalert": False,
+            "invited_by": site_admin_user.id,
+            "notification_daily": False,
+            "notification_weekly": True,
+            "notification_monthly": True,
+            "termsaccepted": False,
+        },
+    )
+
+    assert response.status_code == 401
+
+
+def test_users_edit_no_permission(db: Session, read_only_user_token, client, site_admin_user, view_only_user) -> None:
+    user_id = view_only_user.id
+    email = view_only_user.email
+    terms_accepted = view_only_user.termsaccepted
+    gpg_key = view_only_user.gpgkey
+
+    name = "test" + str(time())
+
+    headers = {"authorization": read_only_user_token}
+    body = {
+        "email": email + "test",
+        "name": name + "test",
+        "termsaccepted": not terms_accepted,
+        "pgpkey": gpg_key + "test",
+    }
+    response = client.put(f"users/{user_id}", headers=headers, json=body)
+
+    assert response.status_code == 401
+
+
+def test_user_view_by_ID_no_permission(db: Session, read_only_user_token, client) -> None:
+    user_id = db.execute(select(User.id)).scalars().first()
+    assert user_id is not None
+
+    headers = {"authorization": read_only_user_token}
+    response = client.get(f"/users/view/{user_id}", headers=headers)
+
+    assert response.status_code == 401
+
+
+def test_users_view_all_unauthorized(db: Session, read_only_user_token, client) -> None:
+    headers = {"authorization": read_only_user_token}
+    response = client.get("/users/view/all", headers=headers)
+
+    assert response.status_code == 401
