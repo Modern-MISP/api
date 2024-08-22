72ecef8a992f1a8a37826adf81cf5a7990f2a798
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-08-22 13:26:47 +0200

    move restSearch tests in separate file

--- tests/api/routers/test_attributes.py
+++ tests/api/routers/test_attributes.py
@@ -320,27 +320,6 @@ def test_delete_selected_attributes_from_existing_event(
     assert response_json["url"] == f"/attributes/deleteSelected/{event_id}"
 
 
-def test_valid_search_attribute_data(db: Session, event, site_admin_user_token, sharing_group, client) -> None:
-    request_body = {"returnFormat": "json", "limit": 100}
-
-    event.sharing_group_id = sharing_group.id
-    db.commit()
-
-    headers = {"authorization": site_admin_user_token}
-    response = client.post("/attributes/restSearch", json=request_body, headers=headers)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert "response" in response_json
-    assert isinstance(response_json["response"]["Attribute"], list)
-
-
-def test_invalid_search_attribute_data(site_admin_user_token, client) -> None:
-    request_body = {"returnFormat": "invalid format"}
-    headers = {"authorization": site_admin_user_token}
-    response = client.post("/attributes/restSearch", json=request_body, headers=headers)
-    assert response.status_code == 404
-
-
 # --- Test attribute statistics
 
 
