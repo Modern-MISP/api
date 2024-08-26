70a1fb4e2c1b5e03547cc0f09ef6e3eae9781d67
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-08-26 11:42:22 +0200

    introduce helper function for compatibility tests

--- tests/compatibility_tests/test_attributes_restSearch.py
+++ tests/compatibility_tests/test_attributes_restSearch.py
@@ -17,20 +17,56 @@ def to_legacy_format(data):
     return data
 
 
-@pytest.mark.asyncio
-async def test_valid_search_attribute_data(db: AsyncSession, attribute, auth_key, client) -> None:
+def get_legacy_modern_diff(http_method, path, body, auth_key, client):
     clear_key, auth_key = auth_key
-    ic(clear_key)
-    request_body = {"returnFormat": "json", "limit": 100, "value": attribute.value}
-
     headers = {"authorization": clear_key, "accept": "application/json"}
-    response = client.post("/attributes/restSearch", json=request_body, headers=headers)
+
+    print("-" * 50)
+    print(f"Calling {path}")
+    ic(body)
+
+    call = getattr(client, http_method)
+    response = call(path, json=body, headers=headers)
     response_json = response.json()
     response_json_in_legacy = to_legacy_format(response_json)
 
-    legacy_response = httpx.post("http://misp-core/attributes/restSearch", headers=headers, json=request_body)
+    call = getattr(httpx, http_method)
+    legacy_response = call(f"http://misp-core{path}", json=body, headers=headers)
     ic(legacy_response)
     legacy_response_json = legacy_response.json()
+    ic("Modern MISP Response")
     ic(response_json)
+    ic("Legacy MISP Response")
     ic(legacy_response_json)
-    assert DeepDiff(response_json_in_legacy, legacy_response_json) == {}
+
+    diff = DeepDiff(response_json_in_legacy, legacy_response_json, verbose_level=2)
+    ic(diff)
+
+    # remove None values added in Modern MISP.
+    # They shouldnt hurt and removing all None
+    # overshoots, MISP is inconsistent when to include what.
+    # Note: We don't want the opposite. If MISP includes None, Modern MISP should do this as well!
+    diff["dictionary_item_removed"] = {k: v for k, v in diff["dictionary_item_removed"].items() if v is not None}
+    if diff["dictionary_item_removed"] == {}:
+        del diff["dictionary_item_removed"]
+
+    return diff
+
+
+@pytest.mark.asyncio
+async def test_valid_search_attribute_data(db: AsyncSession, attribute, auth_key, client) -> None:
+    request_body = {"returnFormat": "json", "limit": 100, "value": attribute.value}
+    path = "/attributes/restSearch"
+
+    assert get_legacy_modern_diff("post", path, request_body, auth_key, client) == {}
+
+
+@pytest.mark.asyncio
+async def test_valid_search_multi_attribute_data(
+    db: AsyncSession, attribute_multi, attribute_multi2, auth_key, client
+) -> None:
+    assert attribute_multi.value != attribute_multi2.value
+    request_body = {"returnFormat": "json", "limit": 100, "value": attribute_multi.value1}
+    path = "/attributes/restSearch"
+
+    assert get_legacy_modern_diff("post", path, request_body, auth_key, client) == {}
