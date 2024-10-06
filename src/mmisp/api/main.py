91967772f888694afc0de9064174fe779d574a9f
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-07 01:14:49 +0200

    use gunicorn, ORJSON, for uvicorn libuv

--- src/mmisp/api/main.py
+++ src/mmisp/api/main.py
@@ -6,6 +6,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
 
 import mmisp.db.all_models  # noqa: F401
 from mmisp.api.config import config
@@ -43,7 +44,7 @@ def init_app(*, init_db: bool = True) -> FastAPI:
     else:
         lifespan = None  # type: ignore
 
-    app = FastAPI(lifespan=lifespan)
+    app = FastAPI(lifespan=lifespan, default_response_class=ORJSONResponse)
 
     app.add_middleware(
         CORSMiddleware,
