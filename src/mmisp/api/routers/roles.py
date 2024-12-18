a068702263e202a2c31e6431945e7735ed414860
Leon Heckh <leon.heckh@gmail.com>
2024-12-18 19:54:01 +0100

    added funktion headers for dvanced role management

--- src/mmisp/api/routers/roles.py
+++ src/mmisp/api/routers/roles.py
@@ -75,7 +75,7 @@ async def get_role_info(
     summary="Add new role",
 )
 async def add_role(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, []))],
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.ADMIN]))],
     db: Annotated[Session, Depends(get_db)],
     body: AddRoleBody,
 ) -> AddRoleResponse:
@@ -103,7 +103,7 @@ async def add_role(
     summary="Delete a role",
 )
 async def delete_role(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, []))],
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.ADMIN]))],
     db: Annotated[Session, Depends(get_db)],
     role_id: Annotated[int, Path(alias="roleId")],
 ) -> DeleteRoleResponse:
@@ -124,7 +124,6 @@ async def delete_role(
     return await None
 
 
-
 @router.put(
     "/admin/roles/edit/{roleId}",
     status_code=status.HTTP_200_OK,
@@ -132,7 +131,7 @@ async def delete_role(
     summary="Edit a role",
 )
 async def update_role(
-    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, []))],
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.ADMIN]))],
     db: Annotated[Session, Depends(get_db)],
     role_id: Annotated[str, Path(alias="roleId")],
     body: EditRoleBody,
@@ -156,7 +155,141 @@ async def update_role(
     return await None
 
 
+@router.post(
+    "/roles/reinstate",
+    status_code=status.HTTP_200_OK,
+    response_model=ReinstateRoleResponse,
+    summary="Reinstate a deleted standard role",
+)
+async def reinstate_role(
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID, [Permission.ADMIN]))],
+    db: Annotated[Session, Depends(get_db)],
+    role_id: Annotated[str, Path(alias="roleId")],
+    body: ReinstateRoleBody,
+) -> ReinstateRoleResponse:
+    """Reinstate a deleted standard role.
 
+    Input:
+
+    - the user's authentication status
+    - the current database
+    - the role id
+    - the requested body containing the name of the requested standard role
+
+    Output:
+
+    - the updated role
+
+    Responses:
+
+    - 200: Successful Response
+    - 422: Validation Error
+    - 403: Forbidden Error
+    - 404: Not Found Error
+    """
+    return await _reinstate_role(auth, db, role_id, body)
+
+
+@router.post(
+    "/roles/restSearch",
+    status_code=status.HTTP_200_OK,
+    response_model=List[FilterRoleResponse],
+    summary="Search roles with filters",
+)
+async def filter_roles(
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.HYBRID))],
+    db: Annotated[Session, Depends(get_db)],
+    body: FilterRoleBody,
+) -> List[FilterRoleResponse]:
+    """Search roles based on filters.
+
+    Input:
+
+    - the user's authentication status
+    - the current database
+    - the requested body containing the requested filter data
+
+    Output:
+
+    - the searched and filtered roles
+
+    Responses:
+
+    - 200: Successful Response
+    - 422: Validation Error
+    - 403: Forbidden Error
+    - 404: Not Found Error
+    """
+    return await _filter_roles(auth, db, body)
+
+
+@router.put(
+    "/admin/users/edit/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=EditUserRoleResponse,
+    summary="Assign or reassign a user to a specific role",
+)
+async def edit_user_role(
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.ADMIN, [Permission.ADMIN]))],
+    db: Annotated[Session, Depends(get_db)],
+    user_id: Annotated[str, Path(alias="userId")],
+    body: EditUserRoleBody,
+) -> EditUserRoleResponse:
+    """Assign or reassign a user to a specific role.
+
+    Input:
+
+    - authentication details
+    - database session
+    - user_id: ID of the user for whom the setting is to be updated
+    - body: new role for updating the user setting
+
+    Output:
+
+    - the updated user
+
+    Responses:
+
+    - 200: Successful Response
+    - 422: Validation Error
+    - 403: Forbidden Error
+    - 404: Not Found Error
+    """
+    return await _edit_user_role(auth, db, user_id, body)
+
+
+@router.put(
+    "/admin/roles/setDefault",
+    status_code=status.HTTP_200_OK,
+    response_model=DefaultRoleResponse,
+    summary="Change the default role",
+)
+async def set_default_role(
+    auth: Annotated[Auth, Depends(authorize(AuthStrategy.ADMIN, [Permission.ADMIN]))],
+    db: Annotated[Session, Depends(get_db)],
+    body: DefaultRoleBody,
+) -> DefaultRoleResponse:
+    """Change the default role (if not changed the default role is 'read only').
+
+    Input:
+
+    - the user's authentication status
+    - the current database
+    - the requested body containing the new default role
+
+    Output:
+
+    - the new default role
+
+    Responses:
+
+    - 200: Successful Response
+    - 422: Validation Error
+    - 403: Forbidden Error
+    - 404: Not Found Error
+
+    """
+    return await _set_default_role(auth, db, body)
 
 
 # --- endpoint logic ---
