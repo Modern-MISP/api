790fd0975a1636209e59fa0a423a80f7be3f98df
Leon Heckh <leon.heckh@gmail.com>
2025-03-06 17:26:33 +0100

    reverted deleation of site admin role

--- tests/compatibility_tests/test_roles.py
+++ tests/compatibility_tests/test_roles.py
@@ -11,10 +11,6 @@ async def test_get_all_roles(db, auth_key, client, site_admin_user_token) -> Non
     path = "/roles"
     request_body = None
 
-    #delete role site_admin with role id 23 that excists for whatever reason and is not needed for this test
-    await db.execute(delete(Role).where(Role.id == 23))
-    await db.commit()
-
     assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
 
 '''
