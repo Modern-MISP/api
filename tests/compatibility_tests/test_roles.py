970b7d398047ec1c97fa0b38776538202d70112f
Leon Heckh <leon.heckh@gmail.com>
2025-03-07 18:01:33 +0100

    added compatibility test for edditing a role

--- tests/compatibility_tests/test_roles.py
+++ tests/compatibility_tests/test_roles.py
@@ -11,12 +11,15 @@ async def test_get_all_roles(db, auth_key, client, site_admin_user_token) -> Non
     assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
 
 
-"""
 @pytest.mark.asyncio
 async def test_edit_role(db, auth_key, client, site_admin_user_token) -> None:
     path = "/admin/roles/edit/5"
-
-    request_body = {"name": "updated_role_name"}
+    
+    request_body = {
+        "perm_add": False,
+        "perm_modify": False,
+        "perm_modify_org": False,
+        "perm_publish": False
+    }
 
     assert get_legacy_modern_diff("put", path, request_body, auth_key, client) == {}
-"""
