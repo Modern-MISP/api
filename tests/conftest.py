b8dc3919d8d045f3e48b3108601db0ea7e75f904
Vincent <vincodev@dev-cube.net>
2025-03-10 14:46:48 +0100

    Committing code out from commit 9e85a938427e06053a36f466b51d80022ceeb9b7

--- tests/conftest.py
+++ tests/conftest.py
@@ -898,7 +898,7 @@ async def access_test_objects(db, site_admin_user, role_read_only):
     db.add(event_read_only_user_2)
     await db.commit()
     await db.refresh(event_read_only_user_2)
-
+    """
     event_dist_comm = generate_event()
     event_dist_comm.org_id = org_no_users
     event_dist_comm.orgc_id = org_no_users
@@ -922,7 +922,7 @@ async def access_test_objects(db, site_admin_user, role_read_only):
     db.add(event_dist_comm_2)
     await db.commit()
     await db.refresh(event_dist_comm_2)
-
+    """
     event_dist_sg = generate_event()
     event_dist_sg.org_id = default_org_id
     event_dist_sg.orgc_id = default_org_id
@@ -1021,8 +1021,8 @@ async def access_test_objects(db, site_admin_user, role_read_only):
         "event_no_access": event_no_access,
         "event_dist_sg": event_dist_sg,
         "event_dist_sg_2": event_dist_sg_2,
-        "event_dist_comm": event_dist_comm,
-        "event_dist_comm_2": event_dist_comm_2,
+       # "event_dist_comm": event_dist_comm,
+       # "event_dist_comm_2": event_dist_comm_2,
         "default_attribute": default_attribute,
         "default_attribute_2": default_attribute_2,
         "attribute_no_access": attribute_no_access,
@@ -1043,8 +1043,8 @@ async def access_test_objects(db, site_admin_user, role_read_only):
     default_event.attribute_count -= 1
     await db.delete(default_attribute)
     default_event.attribute_count -= 1
-    await db.delete(event_dist_comm_2)
-    await db.delete(event_dist_comm)
+   # await db.delete(event_dist_comm_2)
+   # await db.delete(event_dist_comm)
     await db.delete(event_dist_sg_2)
     await db.delete(event_dist_sg)
     await db.delete(event_read_only_user_2)
