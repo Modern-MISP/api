3c66697d868586b74474cc0297eaa6a3cbf2ca2e
Vincent <vincodev@dev-cube.net>
2025-03-12 16:57:28 +0100

    Add test get_event_fail_read_only_user_comm and get_event_success_site_admin

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -56,10 +56,33 @@ async def test_get_event_fail_read_only_user_not_published(access_test_objects, 
 async def test_get_event_success_read_only_user_comm(access_test_objects, client) -> None:
     def preprocessor(modern, legacy):
         del modern["Event"]["event_creator_email"]
-    #    del legacy["Event"]["event_creator_email"]
 
     path = "/events/" + str(access_test_objects["event_dist_comm"].id)
     request_body = {"distribution": 0}
     clear_key = access_test_objects["default_read_only_user_clear_key"]
     auth_key = access_test_objects["default_read_only_user_auth_key"]
     assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client, preprocessor) == {}
+
+
+@pytest.mark.asyncio
+async def test_get_event_fail_read_only_user_comm(access_test_objects, client) -> None:
+    path = "/events/" + str(access_test_objects["event_dist_comm_2"].id)
+    request_body = {"distribution": 0}
+    clear_key = access_test_objects["default_read_only_user_clear_key"]
+    auth_key = access_test_objects["default_read_only_user_auth_key"]
+    assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
+
+
+# @pytest.mark.asyncio
+# async def test_get_event_success_read_only_user_sg(access_test_objects, client) -> None:
+
+
+# @pytest.mark.asyncio
+# async def test_get_event_fail_read_only_user_sg(access_test_objects, client) -> None:
+
+
+@pytest.mark.asyncio
+async def test_get_event_success_site_admin(access_test_objects, auth_key, client) -> None:
+    path = "/events/" + str(access_test_objects["default_event"].id)
+    request_body = {"distribution": 0}
+    assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
