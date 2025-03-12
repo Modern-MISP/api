be096e4900e4a1ddd23a47b9ac58e7880d3e54c9
Vincent <vincodev@dev-cube.net>
2025-03-12 12:59:17 +0100

    Fix erros in test list_all_events_admin

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -12,11 +12,8 @@ async def test_list_all_events_self_created(access_test_objects, client) -> None
     assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
 
 @pytest.mark.asyncio
-async def test_list_all_events_admin(access_test_objects, auth_key, client) -> None:
-    headers = {"authorization": access_test_objects["site_admin_user_token"]}
-    response = client.get("/events", headers=headers)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert isinstance(response_json, list)
-    assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
+async def test_list_all_events_admin(auth_key, client) -> None:
+    path = "/events"
+    request_body = None
+    assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
 
