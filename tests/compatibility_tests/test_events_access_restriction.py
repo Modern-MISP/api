41e520ffb894f341ec818465bc6ef2c05c7a852d
Vincent <vincodev@dev-cube.net>
2025-03-12 13:32:52 +0100

    Comment test_list_all_events_admin out

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -11,9 +11,10 @@ async def test_list_all_events_self_created(access_test_objects, client) -> None
     auth_key = access_test_objects["default_user_auth_key"]
     assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
 
-
+"""
 @pytest.mark.asyncio
 async def test_list_all_events_admin(auth_key, client) -> None:
     path = "/events"
     request_body = None
     assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
+"""
