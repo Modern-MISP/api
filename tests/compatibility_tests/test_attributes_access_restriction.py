78904c2f97ca09469f0823639e015c7dab2e2112
md <micha_duerr@protonmail.com>
2025-03-12 17:38:33 +0100

    add comp test: get_all_attributes

--- tests/compatibility_tests/test_attributes_access_restriction.py
+++ tests/compatibility_tests/test_attributes_access_restriction.py
@@ -1,3 +1,20 @@
 import pytest
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from mmisp.tests.compatibility_helpers import get_legacy_modern_diff
+
+
+@pytest.mark.asyncio
+async def test_get_all_attributes(
+    access_test_objects,
+    client,
+) -> None:
+
+    path = "/attributes"
+    request_body = {}
+    clear_key = access_test_objects["default_read_only_user_clear_key"]
+    auth_key = access_test_objects["default_read_only_user_auth_key"]
+
+    assert get_legacy_modern_diff("get", path, request_body, (clear_key,auth_key), client) == {}
+
