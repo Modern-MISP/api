e651d0e44ed934650b93217773d29dfbd459da3c
md <micha_duerr@protonmail.com>
2025-03-12 18:57:53 +0100

    try comp test_get_all_attributes with just auth_key (from access_test_objects)

--- tests/compatibility_tests/test_attributes_access_restriction.py
+++ tests/compatibility_tests/test_attributes_access_restriction.py
@@ -7,7 +7,6 @@ from mmisp.tests.compatibility_helpers import get_legacy_modern_diff
 
 @pytest.mark.asyncio
 async def test_get_all_attributes(
-    auth_key,
     access_test_objects,
     client,
 ) -> None:
@@ -15,7 +14,7 @@ async def test_get_all_attributes(
     path = "/attributes"
     request_body = {}
    # clear_key = access_test_objects["default_read_only_user_clear_key"]
-   # auth_key = access_test_objects["default_read_only_user_auth_key"]
+    auth_key = access_test_objects["default_read_only_user_auth_key"]
 
     assert get_legacy_modern_diff("get", path, request_body, auth_key, client) == {}
 
