26964259626c0456118c79de08e67e05564a0f59
uxnuk <uxnuk@student.kit.edu>
2025-03-13 16:17:03 +0100

    init tests

--- tests/api/routers/test_worker.py
+++ tests/api/routers/test_worker.py
@@ -8,10 +8,8 @@ from mmisp.api.config import config
 @respx.mock
 @pytest.mark.asyncio
 async def test_get_workers_success(site_admin_user_token, client) -> None:
-    route = respx.get(f"{config.WORKER_URL}/worker/list_workers").mock(
-        return_value=Response(200, json=[{"name": "nameabc", "status": "cba", "queues": ["halts Maus"], "jobCount": 7}])
-    )
-    # tests
+    route = respx.get(f"{config.WORKER_URL}/worker/list_workers").mock(return_value=Response(200, json={}))
+
     response = client.get("/worker/all", headers={"authorization": site_admin_user_token})
 
     assert response.status_code == 200
