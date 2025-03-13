7eddb2b6c6a3c11762a031a778bc9bb30522495f
Vincent <vincodev@dev-cube.net>
2025-03-13 16:57:19 +0100

    Add legacy misp test list_all_events_read_only_user

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -12,8 +12,13 @@ async def test_list_all_events_self_created(access_test_objects, client) -> None
     assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
 
 
-#@pytest.mark.asyncio
-#async def test_list_all_events_read_only_user(access_test_objects, client) -> None:
+@pytest.mark.asyncio
+async def test_list_all_events_read_only_user(access_test_objects, client) -> None:
+    path = "/events"
+    request_body = None
+    clear_key = access_test_objects["default_read_only_user_clear_key"]
+    auth_key = access_test_objects["default_read_only_user_auth_key"]
+    assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
 
 @pytest.mark.asyncio
 async def test_list_all_events_admin(auth_key, client) -> None:
