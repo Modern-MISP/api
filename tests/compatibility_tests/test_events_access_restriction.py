723baca5c23832f1fe18b6a56f30d79be0d728d7
Vincent <vincodev@dev-cube.net>
2025-03-14 19:58:08 +0100

    Add debug to test_get_event_success_read_only_user_sg

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -81,7 +81,8 @@ async def test_get_event_fail_read_only_user_comm(access_test_objects, client) -
 @pytest.mark.asyncio
 async def test_get_event_success_read_only_user_sg(access_test_objects, client) -> None:
     path = "/events/" + str(access_test_objects["event_dist_sg"].id)
-    request_body = {}
+    print("Event dist sg: ", access_test_objects["event_dist_sg"].__dict__)
+    request_body = None
     clear_key = access_test_objects["default_sharing_group_user_clear_key"]
     auth_key = access_test_objects["default_sharing_group_user_auth_key"]
     assert get_legacy_modern_diff("get", path, request_body, (clear_key, auth_key), client) == {}
