e21a006a4133e05646184fd214071dfc211db9e1
md <micha_duerr@protonmail.com>
2025-03-14 09:03:10 +0100

    move first valid_search_test

--- tests/compatibility_tests/test_attributes_restSearch.py
+++ tests/compatibility_tests/test_attributes_restSearch.py
@@ -60,3 +60,16 @@ async def test_valid_search_galaxy_tag_attribute_data(
     path = "/attributes/restSearch"
 
     assert get_legacy_modern_diff("post", path, request_body, auth_key, client) == {}
+
+
+@pytest.mark.asyncio
+async def test_valid_search_attribute_data_site_admin(
+    db: AsyncSession, auth_key, client) -> None:
+    def preprocessor(modern, legacy):
+        del modern["Event"]["Tag"]
+        del modern["Event"]["Attribute"]["Tag"]
+
+    path = "/events/restSearch"
+    request_body = {"returnFormat": "json", "limit": 100, "distribution": 0}
+    assert get_legacy_modern_diff("post", path, request_body, auth_key, client) == {}
+
