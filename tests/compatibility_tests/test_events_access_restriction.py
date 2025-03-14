01c1a628f612dfbea46ce1784fdd3ad32d75e7c9
md <micha_duerr@protonmail.com>
2025-03-14 08:14:57 +0100

    fix test_valid_search_attribute_data_site_admin

--- tests/compatibility_tests/test_events_access_restriction.py
+++ tests/compatibility_tests/test_events_access_restriction.py
@@ -137,7 +137,7 @@ async def test_valid_search_attribute_data_site_admin(auth_key, client) -> None:
         del modern["Event"]["Attribute"]["Tag"]
 
     path = "/events/restSearch"
-    request_body = {"returnFormat": "json", "limit": 100, "distribution": 0, "event_creator_mail": "test.com"}
+    request_body = {"returnFormat": "json", "limit": 100, "distribution": 0}
     assert get_legacy_modern_diff("post", path, request_body, auth_key, client) == {}
 
 
