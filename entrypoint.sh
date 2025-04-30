25e4fe896b7cb44dc98f54b600be8cd3fff8f5e8
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-30 15:52:24 +0200

    make worker in entrypoint.sh configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 
 [ -n "${SETUP_DB}" ] && mmisp-db setup --create_init_values=False
+[ -z "${WORKER_COUNT}" ] && WORKER_COUNT=4
 
-gunicorn mmisp.api.main:app -b 0.0.0.0:4000 -w 4 -k uvicorn_worker.UvicornWorker
+gunicorn mmisp.api.main:app -b 0.0.0.0:4000 -w "$WORKER_COUNT" -k uvicorn_worker.UvicornWorker
